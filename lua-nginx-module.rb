f428733db8db0903fa26804bc5def2d7ede0cc14
Marc Qualie <marc@marcqualie.com>
2013-06-29 19:11:25 +0100

    Updated lua module to version v0.8.3

--- lua-nginx-module.rb
+++ lua-nginx-module.rb
@@ -3,8 +3,8 @@ require 'formula'
 class LuaNginxModule < Formula
 
   homepage 'https://github.com/chaoslawful/lua-nginx-module'
-  url 'https://github.com/chaoslawful/lua-nginx-module/archive/v0.8.1.tar.gz'
-  sha1 '05b5c3904a0e674f6dd3980ba6d210fbae91b38e'
+  url 'https://github.com/chaoslawful/lua-nginx-module/archive/v0.8.3.tar.gz'
+  sha1 '2c1ad0a2a3018fe85322430ff0c824e5edf9b37d'
 
   depends_on "luajit"
 
