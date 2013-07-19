93dd8e09e9330a66d0ca4f176b4b58c4cd300557
Marc Qualie <marc@marcqualie.com>
2013-07-19 11:24:43 +0100

    lua-nginx-module v0.8.5

--- lua-nginx-module.rb
+++ lua-nginx-module.rb
@@ -3,8 +3,8 @@ require 'formula'
 class LuaNginxModule < Formula
 
   homepage 'https://github.com/chaoslawful/lua-nginx-module'
-  url 'https://github.com/chaoslawful/lua-nginx-module/archive/v0.8.3.tar.gz'
-  sha1 '2c1ad0a2a3018fe85322430ff0c824e5edf9b37d'
+  url 'https://github.com/chaoslawful/lua-nginx-module/archive/v0.8.5.tar.gz'
+  sha1 'e7853e8f645f56bfb4347869528b33e0af23fed3'
 
   depends_on "luajit"
 
@@ -12,7 +12,4 @@ class LuaNginxModule < Formula
     (share+'lua-nginx-module').install Dir['*']
   end
 
-  test do
-    system "false"
-  end
 end
