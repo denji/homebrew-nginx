ad55a980772bb73d198546f8adf99676220f48a8
denji <denji@localhost>
2013-09-16 15:53:15 +0300

    Depends openldap

--- auth-ldap-nginx-module.rb
+++ auth-ldap-nginx-module.rb
@@ -7,6 +7,8 @@ class AuthLdapNginxModule < Formula
   sha1 '663c521a487ff5b6c87c59b8fe5a7677410a30b0'
   version '0.0.1'
 
+  depends_on "openldap"
+
   def install
     (share+'auth-ldap-nginx-module').install Dir['*']
   end
