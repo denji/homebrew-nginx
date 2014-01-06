7429a4bdef1705efbca2cb280069006221001097
woolf-wen <woolf.wen@gmail.com>
2014-01-06 09:34:50 +0800

    Update upload-progress-nginx-module.rb
    update script

--- upload-progress-nginx-module.rb
+++ upload-progress-nginx-module.rb
@@ -1,11 +1,14 @@
+require 'formula'
+
 class UploadprogressNginxModule < Formula
+  
   homepage 'http://wiki.nginx.org/HttpUploadProgressModule'
   url 'http://github.com/downloads/masterzen/nginx-upload-progress-module/nginx_uploadprogress_module-0.9.0.tar.gz'
   sha1 '93aadaf6065e1e8afd9f14b9291a66819d3aa4c0'
   version '0.9.0'
 
   def install
-    (share+'upload-nginx-module').install Dir['*']
+    (share+'upload-progress-nginx-module').install Dir['*']
   end
   
 end
