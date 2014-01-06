fe0ac819cee68192f23cc08c5e2c14eeae03b737
woolf-wen <woolf.wen@gmail.com>
2014-01-06 08:53:06 +0800

    Update update-progress-nginx-module.rb
    rename clase name

--- update-progress-nginx-module.rb
+++ update-progress-nginx-module.rb
@@ -1,4 +1,4 @@
-class NginxUploadprogressModule < Formula
+class UploadprogressNginxModule < Formula
   homepage 'http://wiki.nginx.org/HttpUploadProgressModule'
   url 'http://github.com/downloads/masterzen/nginx-upload-progress-module/nginx_uploadprogress_module-0.9.0.tar.gz'
   sha1 '93aadaf6065e1e8afd9f14b9291a66819d3aa4c0'
