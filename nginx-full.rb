9aa3fb2380803298ed4c8871b1c85b7d4c294337
denji <denji@localhost>
2014-01-07 17:25:36 +0200

    Fix prefix "Substitutions Filter Module"

--- nginx-full.rb
+++ nginx-full.rb
@@ -77,7 +77,7 @@ class NginxFull < Formula
       ['with-mogilefs-module',    nil,                           'Compile with support for HTTP MogileFS Module'],
       ['with-mp4-h264-module',    nil,                           'Compile with support for HTTP MP4/H264 Module'],
       ['with-notice-module',      nil,                           'Compile with support for HTTP Notice Module'],
-      ['with-subs-filter',        nil,                           'Compile with support for Substitutions Filter Module'],
+      ['with-subs-filter-module', nil,                           'Compile with support for Substitutions Filter Module'],
       ['with-upload-module',      nil,                           'Compile with support for Upload module'],
       ['with-upload-progress-module', nil,                       'Compile with support for Upload Progrress module'],
       # Internal modules
