3860e06bc01d54c0e50f9bee6f42fc3816cb4eb6
denji <denji@localhost>
2014-01-07 18:36:37 +0200

    Fix move path

--- autols-nginx-module.rb
+++ autols-nginx-module.rb
@@ -6,7 +6,7 @@ class AutolsNginxModule < Formula
   sha1 'b37901b2418f688ffb84f909d2fa8e165fcb258d'
 
   def install
-    (share+'autols-nginx-module').mv 'ngx_http_autols_module/*', "#{HOMEBREW_PREFIX}/share/autols-nginx-module/"
+    (share+'autols-nginx-module').mv 'ngx_http_autols_module/*', './'
     (share+'autols-nginx-module').rm_rf 'ngx_http_autols_module'
     (share+'autols-nginx-module').rm 'README-DEMO.png'
     (share+'autols-nginx-module').install Dir['*']
