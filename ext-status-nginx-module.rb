d196072a4937f93eafe05d22e1e3f2d1375ee761
denji <denji@localhost>
2014-01-07 19:05:36 +0200

    Extended Status Patch

--- ext-status-nginx-module.rb
+++ ext-status-nginx-module.rb
@@ -5,6 +5,10 @@ class ExtStatusNginxModule < Formula
   url 'https://github.com/denji/ngx_http_extended_status_module/archive/v1.0.tar.gz'
   sha1 '884f39921be512a4939be93bb0a5b64bf68fff06'
 
+  def patches
+    "https://raw.github.com/denji/ngx_http_extended_status_module/master/extended_status-1.5.2.patch"
+  end
+
   def install
     (share+'ext-status-nginx-module').install Dir['*']
   end
