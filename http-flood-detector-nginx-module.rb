58d5724a4c03960d4346e5cf6ace8151fe198db3
denji <denieldfu@gmail.com>
2014-01-09 00:13:29 +0200

    Lost link

--- http-flood-detector-nginx-module.rb
+++ http-flood-detector-nginx-module.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class HttpFloodDetectorNginxModule < Formula
   homepage 'https://github.com/suehiro/ngx_http_flood_detector_module'
-  url 'https://github.com/alexstup/ngx_http_var_request_speed/archive/ad1abbc81b.tar.gz'
-  sha1 ''
+  url 'https://github.com/suehiro/ngx_http_flood_detector_module/archive/b66fb66c23.tar.gz'
+  sha1 '8b3a9de84cf6c326190b030fb78da10194a2942b'
 
    if !build.with? 'with-status'
     cause "http-flood-detector-nginx-module: Stub Status module is required `--with-status`"
