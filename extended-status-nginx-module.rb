5ec1acd71c58a0a8967e0c596c1e1c19e0a9c4e8
denji <denji@localhost>
2014-01-08 22:06:10 +0200

    Rename to real name

--- extended-status-nginx-module.rb
+++ extended-status-nginx-module.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class ExtStatusNginxModule < Formula
+class ExtendedStatusNginxModule < Formula
   homepage 'https://github.com/denji/ngx_http_extended_status_module'
   url 'https://github.com/denji/ngx_http_extended_status_module/archive/v1.0.tar.gz'
   sha1 '884f39921be512a4939be93bb0a5b64bf68fff06'
@@ -10,6 +10,6 @@ class ExtStatusNginxModule < Formula
   end
 
   def install
-    (share+'ext-status-nginx-module').install Dir['*']
+    (share+'extended-status-nginx-module').install Dir['*']
   end
 end
--- nginx-full.rb
+++ nginx-full.rb
@@ -85,7 +85,7 @@ class NginxFull < Formula
       ['with-autols-module',      nil,                           'Compile with support for Flexible Auto Index module'],
       ['with-auto-keepalive-module', nil,                        'Compile with support for Auto Disable KeepAlive module'],
       ['with-ustats-module',      nil,                           'Compile with support for Upstream Statistics (HAProxy style) module'],
-      ['with-ext-status-module',  nil,                           'Compile with support for Extended Status module'],
+      ['with-extended-status-module',  nil,                       'Compile with support for Extended Status module'],
       ['with-no-pool-nginx',      nil,                           'Patch disable nginx pool machanism & valgrind memcheck to detect memory issues'],
       # Internal modules
       ['with-webdav',            'with-http_dav_module',         'Compile with support for WebDAV module'],
@@ -275,7 +275,7 @@ class NginxFull < Formula
     args << "--add-module=#{HOMEBREW_PREFIX}/share/ustats-nginx-module" if build.include? "with-ustats-module"
 
     # Extended Status Module
-    args << "--add-module=#{HOMEBREW_PREFIX}/share/ext-status-nginx-module" if build.include? "with-ext-status-module"
+    args << "--add-module=#{HOMEBREW_PREFIX}/share/extended-status-nginx-module" if build.include? "with-extended-status-module"
 
     if build.head?
       system "./auto/configure", *args
