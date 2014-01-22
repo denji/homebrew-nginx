55d3a6f39182030b25fd2ace6426a61092d57d29
denji <denji0k@gmail.com>
2014-01-22 17:34:26 +0200

    lua-nginx v0.9.4 and ngx_devkit v0.2.19 #41

--- ngx-devel-kit.rb
+++ ngx-devel-kit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class NgxDevelKit < Formula
   homepage 'https://github.com/simpl/ngx_devel_kit'
-  url 'https://github.com/simpl/ngx_devel_kit/archive/v0.2.18.tar.gz'
-  sha1 'e21ba642f26047661ada678b21eef001ee2121d8'
+  url 'https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz'
+  sha1 '888635e80a8a0e6242b8e9b684ff60ffa70845a2'
 
   def install
     (share+'ngx-devel-kit').install Dir['*']
