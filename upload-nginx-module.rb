d5b656e736af650f8a68cf8bce5e94dfadac8ca2
Marc Qualie <marc@marcqualie.com>
2014-03-18 22:25:54 +0000

    Updated sha1 for upload-module Fixes #46

--- upload-nginx-module.rb
+++ upload-nginx-module.rb
@@ -3,7 +3,7 @@ require 'formula'
 class UploadNginxModule < Formula
   homepage 'http://wiki.nginx.org/HttpUploadModule'
   url 'https://github.com/vkholodkov/nginx-upload-module/archive/2.2.tar.gz'
-  sha1 'c8cbeddce12e80e4103e6a3a86754a331bd7ab8a'
+  sha1 'a7d31ac0358b86ff12a07bcda1e9a8714118c2bb'
   version '2.2.0'
 
   def install
