f5e49d4e618baf61fef95c4a993919dff87dbd46
Marc Qualie <marc@marcqualie.com>
2014-03-27 15:21:34 +0000

    Include nginx-constants as a method on nginx-full Fixes #47

--- nginx-full.rb
+++ nginx-full.rb
@@ -1,8 +1,6 @@
 require 'formula'
-require 'nginx-constants'
 
 class NginxFull < Formula
-  include NginxConstants
 
   homepage 'http://nginx.org/'
   url 'http://nginx.org/download/nginx-1.4.7.tar.gz'
@@ -17,6 +15,53 @@ class NginxFull < Formula
 
   env :userpaths
 
+  def self.third_party_modules
+    {
+      'lua' => 'Compile with support for LUA module',
+      'echo' => 'Compile with support for Echo Module',
+      'auth-digest' => 'Compile with support for Auth Digest Module',
+      'set-misc' => 'Compile with support for Set Misc Module',
+      'redis2' => 'Compile with support for Redis2 Module',
+      'array-var' => 'Compile with support for Array Var Module',
+      'accept-language' => 'Compile with support for Accept Language Module',
+      'accesskey' => 'Compile with support for HTTP Access Key Module',
+      'auth-ldap' => 'Compile with support for Auth LDAP Module',
+      'auth-pam' => 'Compile with support for Auth PAM Module',
+      'cache-purge' => 'Compile with support for Cache Purge Module',
+      'ctpp2' => 'Compile with support for CT++ Module',
+      'headers-more' => 'Compile with support for Headers More Module',
+      'tcp-proxy' => 'Compile with support for TCP proxy',
+      'dav-ext' => 'Compile with support for HTTP WebDav Extended Module',
+      'eval' => 'Compile with support for Eval Module',
+      'fancyindex' => 'Compile with support for Fancy Index Module',
+      'mogilefs' => 'Compile with support for HTTP MogileFS Module',
+      'mp4-h264' => 'Compile with support for HTTP MP4/H264 Module',
+      'notice' => 'Compile with support for HTTP Notice Module',
+      'subs-filter' => 'Compile with support for Substitutions Filter Module',
+      'upload' => 'Compile with support for Upload module',
+      'upload-progress' => 'Compile with support for Upload Progrress module',
+      'php-session' => 'Compile with support for Parse PHP Sessions module',
+      'anti-ddos' => 'Compile with support for Anti-DDoS module',
+      'captcha' => 'Compile with support for Captcha module',
+      'autols' => 'Compile with support for Flexible Auto Index module',
+      'auto-keepalive' => 'Compile with support for Auto Disable KeepAlive module',
+      'ustats' => 'Compile with support for Upstream Statistics (HAProxy style) module',
+      'extended-status' => 'Compile with support for Extended Status module',
+      'upstream-hash' => 'Compile with support for Upstream Hash Module',
+      'consistent-hash' => 'Compile with support for Consistent Hash Upstream module',
+      'healthcheck' => 'Compile with support for Healthcheck Module',
+      'log-if' => 'Compile with support for Log-if Module',
+      'txid' => 'Compile with support for Sortable Unique ID',
+      'upstream-order' => 'Compile with support for Order Upstream module',
+      'var-req-speed' => 'Compile with support for Var Request-Speed module',
+      'http-flood-detector' => 'Compile with support for Var Flood-Threshold module',
+      'http-remote-passwd' => 'Compile with support for Remote Basic Auth password module',
+      'realtime-req' => 'Compile with support for Realtime Request module',
+      'counter-zone' => 'Compile with support for Realtime Counter Zone module',
+      'mod-zip' => 'Compile with support for HTTP Zip Module',
+    }
+  end
+
   depends_on 'pcre'
   depends_on 'passenger' => :optional
   # SPDY needs openssl >= 1.0.1 for NPN; see:
@@ -30,7 +75,7 @@ class NginxFull < Formula
   depends_on "valgrind" if build.include? 'without-pool-nginx'
 
   # register third party flags
-  THIRD_PARTY.each { | name, desc |
+  self.third_party_modules.each { | name, desc |
     depends_on "#{name}-nginx-module" if build.include? "with-#{name}-module"
   }
 
@@ -38,7 +83,7 @@ class NginxFull < Formula
 
   # Options
   def options_array
-    THIRD_PARTY.collect { | name, desc |
+    self.class.third_party_modules.collect { | name, desc |
       ["with-#{name}-module", nil, desc]
     } + [
       ['with-passenger',         nil,                            'Compile with support for Phusion Passenger module'],
@@ -158,7 +203,7 @@ class NginxFull < Formula
     end
 
     # add third party flags
-    args += THIRD_PARTY.select { | name, desc |
+    args += self.class.third_party_modules.select { | name, desc |
       build.with? "#{name}-module"
     }.collect { | name, desc |
       ohai "--with-#{name}-module"
