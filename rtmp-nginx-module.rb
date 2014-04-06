18f514645e51e78c992186ff42afedc0618e99fe
Nikolay Murga <work@murga.kiev.ua>
2014-04-06 18:23:40 +0300

    Chnage version to '1.1.4b'

--- rtmp-nginx-module.rb
+++ rtmp-nginx-module.rb
@@ -3,7 +3,7 @@ class RtmpNginxModule < Formula
   homepage 'https://github.com/arut/nginx-rtmp-module'
   url 'https://github.com/arut/nginx-rtmp-module/archive/812e2fd7b1888c3142e34cc0e687401fa088e93d.tar.gz'
   sha1 'fc7c9909bc1f4870d136378309b4021838f2a14c'
-  version 'v1.1.4'
+  version '1.1.4b'
 
   def install
     (share+'rtmp-nginx-module').install Dir['*']
