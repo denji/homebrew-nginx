2516ded6708714d5860eb17269cf584ebe51daaf
Emilian Stoilkov <stoilkov@users.noreply.github.com>
2014-04-21 14:20:40 +0300

    Update subs-filter-nginx-module.rb
    wrong checksum.

--- subs-filter-nginx-module.rb
+++ subs-filter-nginx-module.rb
@@ -3,7 +3,7 @@ require 'formula'
 class SubsFilterNginxModule < Formula
   homepage 'https://github.com/yaoweibin/ngx_http_substitutions_filter_module'
   url 'https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/master.tar.gz'
-  sha1 '8ac8efa385c8b0fcfcd6a7505697bcfe498039e3'
+  sha1 'e2dd7540314b6629ab9919de6566eb2ab85c03d0'
   version '0.6.3' # devel version without pcre depend
 
   def install
