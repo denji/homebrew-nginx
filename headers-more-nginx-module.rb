0f8baf46e5ab96b2094fdcbb7a274de1d57a5453
Denis Denisov <denji0k@gmail.com>
2014-05-30 08:34:12 +0300

    Changed the url headers-more-nginx-module

--- headers-more-nginx-module.rb
+++ headers-more-nginx-module.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class HeadersMoreNginxModule < Formula
-  homepage 'https://github.com/agentzh/headers-more-nginx-module'
-  url 'https://github.com/agentzh/headers-more-nginx-module/archive/v0.22.tar.gz'
-  sha1 '9e574fb0a3cacb4229fa2fe93d02bac0d43e431f'
+  homepage 'https://github.com/openresty/headers-more-nginx-module'
+  url 'https://github.com/openresty/headers-more-nginx-module/archive/v0.25.tar.gz'
+  sha1 '514bc3df30b24eb0a06533f1ebaa579b898990f5'
 
   def install
     (share+'headers-more-nginx-module').install Dir['*']
