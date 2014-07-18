ca662a781e05a67376e737e8e74a76b0631c08bc
Denis Denisov <denji@users.noreply.github.com>
2014-07-18 05:18:43 +0400

    nginx 1.7.3 mainline

--- nginx-full.rb
+++ nginx-full.rb
@@ -7,8 +7,8 @@ class NginxFull < Formula
   sha1 '00eed38652d2cee36cc91a395f6703584658bb23'
 
   devel do
-    url 'http://nginx.org/download/nginx-1.7.2.tar.gz'
-    sha1 '2537cba3c9c7f70fcfe5732b46ace38bf2a161c1'
+    url 'http://nginx.org/download/nginx-1.7.3.tar.gz'
+    sha1 '56cd029f1fba6965433327578ad195f36d27114d'
   end
 
   head 'http://hg.nginx.org/nginx/', :using => :hg
