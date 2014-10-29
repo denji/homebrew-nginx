355b745fa9bd8f38868043c179939e03bf53110b
Denis Denisov <denji@users.noreply.github.com>
2014-10-29 17:57:54 +0200

    upstream-hash-nginx-module-0.2
    SHA1 mismatch when installation

--- Formula/upstream-hash-nginx-module.rb
+++ Formula/upstream-hash-nginx-module.rb
@@ -3,8 +3,8 @@ require "formula"
 class UpstreamHashNginxModule < Formula
   homepage "https://github.com/evanmiller/nginx_upstream_hash"
   url "https://github.com/evanmiller/nginx_upstream_hash/archive/master.tar.gz"
-  sha1 "359c3e7f52266763301b29bdf07edfab3d7c32d9"
-  version "0.1"
+  sha1 "daf6650a36e197932670052cd9b8fe0db8230097"
+  version "0.2"
 
   def install
     (share+"upstream-hash-nginx-module").install Dir["*"]
