3e83354367329cf155bbd0d0f51f72d21b834437
Jonathan Seidner <jonathan@crosswise.com>
2014-11-04 12:09:06 +0200

    updated sha1 for log-if module

--- Formula/log-if-nginx-module.rb
+++ Formula/log-if-nginx-module.rb
@@ -3,7 +3,7 @@ require "formula"
 class LogIfNginxModule < Formula
   homepage "https://github.com/cfsego/ngx_log_if"
   url "https://github.com/cfsego/ngx_log_if/archive/master.tar.gz"
-  sha1 "8dfb8c0f9358821377c0dea810a49a026aecd405"
+  sha1 "a337ae59efa7ef1d8a544706e73cb4b004774c56"
   version "0.1"
 
   def install
