1a7c92f73516322e9aa8e471f156b8b74d986249
Denis Denisov <denji@users.noreply.github.com>
2014-11-09 19:37:59 +0200

    push-stream-nginx-module sha1 #74

--- Formula/push-stream-nginx-module.rb
+++ Formula/push-stream-nginx-module.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class PushStreamNginxModule < Formula
   homepage "https://github.com/wandenberg/nginx-push-stream-module"
-  url "https://github.com/wandenberg/nginx-push-stream-module/archive/master.zip"
-  sha1 "17812cd68297c67a595d6a580f138cc9cc382927"
+  url "https://github.com/wandenberg/nginx-push-stream-module/archive/master.tar.gz"
+  sha1 "9d46577c650525b23161dd89d37ca9454dde1f0e"
   version "0.4.1-pre"	# has commit 467376d3c2 fixing clang link errors
 
   def install
