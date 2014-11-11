fb95076533e0af3ca6b0716a2ba957f8d9cb133b
Mike Tigas <mike@tig.as>
2014-11-11 16:44:10 -0500

    update dep

--- Formula/subs-filter-nginx-module.rb
+++ Formula/subs-filter-nginx-module.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class SubsFilterNginxModule < Formula
   homepage "https://github.com/yaoweibin/ngx_http_substitutions_filter_module"
-  url "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/master.tar.gz"
-  sha1 "e2dd7540314b6629ab9919de6566eb2ab85c03d0"
-  version "0.6.3" # devel version without pcre depend
+  url "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/v0.6.4.tar.gz"
+  sha1 "4b586a95411216fe64811758f05975ccf450a490"
+  version "0.6.4"
 
   def install
     (share+"subs-filter-nginx-module").install Dir["*"]
