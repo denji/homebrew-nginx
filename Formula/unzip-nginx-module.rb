4af76dd009ea78e11286995d3833ce5f77efe824
Denis Denisov <denji0k@gmail.com>
2014-12-10 02:40:07 +0200

    unzip revision 1

--- Formula/unzip-nginx-module.rb
+++ Formula/unzip-nginx-module.rb
@@ -3,8 +3,9 @@ require "formula"
 class UnzipNginxModule < Formula
   homepage "https://github.com/youzee/nginx-unzip-module"
   url "https://github.com/youzee/nginx-unzip-module/archive/03e582a2e99d2dd4bbd7f26d3f3d4a8e4a5d23f4.tar.gz"
-  sha1 "624cfc61fc4bb4f76c28f5590377897c04538f5b"
-  version "0.1-pre"
+  sha1 "b6f97d46d114ca432162dc909f34f6d4a2741c1b"
+  version "0.1"
+  revision 1
 
   def install
     (share+"unzip-nginx-module").install Dir["*"]
