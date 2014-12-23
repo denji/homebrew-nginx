0501401b3fa35a1fd5b6dac1c413b2e884fafbc8
Denis Denisov <denji0k@gmail.com>
2014-12-23 22:05:23 +0200

    nginx 1.7.9 mainline

--- Formula/nginx-full.rb
+++ Formula/nginx-full.rb
@@ -7,8 +7,8 @@ class NginxFull < Formula
   sha1 "1a5458bc15acf90eea16353a1dd17285cf97ec35"
 
   devel do
-    url "http://nginx.org/download/nginx-1.7.8.tar.gz"
-    sha1 "e8220f42ec9543f81ea4b96bfd2b242c1e4dae49"
+    url "http://nginx.org/download/nginx-1.7.9.tar.gz"
+    sha1 "44ef4770db281cea26a1b2e66c0e34193e1d1f1f"
   end
 
   head "http://hg.nginx.org/nginx/", :using => :hg
