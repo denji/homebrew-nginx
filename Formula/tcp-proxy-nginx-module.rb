d21f49d4588d4ceaf10c4a0b301323624dbcf49c
Denis Denisov <denji0k@gmail.com>
2015-02-06 12:53:02 +0200

    tcp-proxy patches v0.4.5 branch

--- Formula/tcp-proxy-nginx-module.rb
+++ Formula/tcp-proxy-nginx-module.rb
@@ -2,7 +2,7 @@ class TcpProxyNginxModule < Formula
   homepage "https://github.com/yaoweibin/nginx_tcp_proxy_module"
   url "https://github.com/yaoweibin/nginx_tcp_proxy_module/archive/v0.4.5.tar.gz"
   sha1 "7060c16dba7f31a22462d1a1e3ac97d81c59aef2"
-  
+
   def install
     (share+"tcp-proxy-nginx-module").install Dir["*"]
   end
