7ec51c3b743c9e0ed84a2ee7e99566f189ec2446
Tei1988 <github@grifola.xyz>
2015-03-25 09:52:40 +0900

    fix indicated matters `brew audit small-light-nginx-module` command shows

--- Formula/small-light-nginx-module.rb
+++ Formula/small-light-nginx-module.rb
@@ -1,16 +1,13 @@
 class SmallLightNginxModule < Formula
   homepage "https://github.com/cubicdaiya/ngx_small_light"
   url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.6.6.tar.gz"
-  sha1 "460354c5ff91f8b8f0a5aff435889f0b5d9648cb"
-  version "0.6.6"
+  sha256 "e7852ab5f480bc72f21939ffa5ddff7dcb04e4a6750d5b6ebac874f710bd6f6a"
 
-  option "with-gd", "enable GD"
-  option "with-imlib2", "enable Imlib2"
-
-  depends_on "pkg-config"
   depends_on "imagemagick"
-  depends_on "gd" if build.with? "gd"
-  depends_on "imlib2" if build.with? "imlib2"
+  depends_on "pcre"
+  depends_on "pkg-config" => :build
+  depends_on "gd" => :optional
+  depends_on "imlib2" => :optional
 
   def install
     args = []
