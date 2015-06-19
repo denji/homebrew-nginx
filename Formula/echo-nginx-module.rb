34e8b4d05562115efb0321d5af7cbf3e33e9490c
Denis Denisov <denji0k@gmail.com>
2015-06-20 01:51:16 +0300

    echo-nginx-module 0.57

--- Formula/echo-nginx-module.rb
+++ Formula/echo-nginx-module.rb
@@ -1,7 +1,9 @@
 class EchoNginxModule < Formula
+  desc "Brings echo/sleep/time/exec and more shell-style"
   homepage "https://github.com/openresty/echo-nginx-module"
-  url "https://github.com/openresty/echo-nginx-module/archive/v0.54.tar.gz"
-  sha1 "35c1547c8366ec1c53ea33f95058b187674c7f2b"
+  url "https://github.com/openresty/echo-nginx-module/archive/v0.57.tar.gz"
+  sha256 "8467237ca0fae74ca7a32fbd34fc6044df307098415d48068214c9c235695a07"
+  head "https://github.com/openresty/echo-nginx-module.git"
 
   def install
     (share+"echo-nginx-module").install Dir["*"]
