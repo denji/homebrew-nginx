bb9d43f6f11fa0a9fb12d5a15d6982a647d837e5
Denis Denisov <denji0k@gmail.com>
2015-06-20 02:34:42 +0300

    upload-progress-nginx-module 0.9.1

--- Formula/upload-progress-nginx-module.rb
+++ Formula/upload-progress-nginx-module.rb
@@ -1,8 +1,9 @@
 class UploadProgressNginxModule < Formula
+  desc "implementing an upload progress for nginx"
   homepage "http://wiki.nginx.org/HttpUploadProgressModule"
-  url "http://github.com/downloads/masterzen/nginx-upload-progress-module/nginx_uploadprogress_module-0.9.0.tar.gz"
-  sha1 "93aadaf6065e1e8afd9f14b9291a66819d3aa4c0"
-  version "0.9.0"
+  url "https://github.com/masterzen/nginx-upload-progress-module/archive/v0.9.1.tar.gz"
+  sha256 "99ec072cca35cd7791e77c40a8ded41a7a8c1111e057be26e55fba2fdf105f43"
+  head "https://github.com/masterzen/nginx-upload-progress-module.git"
 
   def install
     (share+"upload-progress-nginx-module").install Dir["*"]
