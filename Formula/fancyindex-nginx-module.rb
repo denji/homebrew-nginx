e6069b0a51b217a1168855e6642936bc3477b2bb
Denis Denisov <denji0k@gmail.com>
2015-06-20 01:57:37 +0300

    fancyindex-nginx-module 0.3.5

--- Formula/fancyindex-nginx-module.rb
+++ Formula/fancyindex-nginx-module.rb
@@ -1,7 +1,9 @@
 class FancyindexNginxModule < Formula
+  desc "Fancy indexes module for the Nginx"
   homepage "https://github.com/aperezdc/ngx-fancyindex"
-  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.3.2.tar.gz"
-  sha1 "98c3c361ac163a49bada0dc25e81f7575cb8ff85"
+  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.3.5.tar.gz"
+  sha256 "e0998e83be58bc5787fa9243a76a9d21de3cd01088d3cf63177ba88a562634a1"
+  head "https://github.com/aperezdc/ngx-fancyindex.git"
 
   def install
     (share+"fancyindex-nginx-module").install Dir["*"]
