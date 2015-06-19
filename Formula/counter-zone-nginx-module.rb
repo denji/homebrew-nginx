53bb8649ed7142e8a342c6581fe6f1ee0fd21b22
Denis Denisov <denji0k@gmail.com>
2015-06-20 01:35:39 +0300

    counter-zone-nginx-module 4be9e36

--- Formula/counter-zone-nginx-module.rb
+++ Formula/counter-zone-nginx-module.rb
@@ -1,7 +1,8 @@
 class CounterZoneNginxModule < Formula
+  desc "Gives realtime counters using NGINX config"
   homepage "https://github.com/theromis/ngx_counter_zone"
-  url "https://github.com/theromis/ngx_counter_zone/archive/dd78fcb781.tar.gz"
-  sha1 "ed97ea0503eefb0aa0e2c3ac3924fb9b606d2eb5"
+  url "https://github.com/theromis/ngx_counter_zone/archive/4be9e36.tar.gz"
+  sha256 "9b841ed09ec39cbe27dab16e017433418c0546fba58bc48d853bed0dcae9e322"
 
   def install
     (share+"counter-zone-nginx-module").install Dir["*"]
