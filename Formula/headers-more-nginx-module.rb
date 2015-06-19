31ea25932c4e196b5e354edcd1a159617a0382b5
Denis Denisov <denji0k@gmail.com>
2015-06-20 02:00:37 +0300

    headers-more-nginx-module 0.26

--- Formula/headers-more-nginx-module.rb
+++ Formula/headers-more-nginx-module.rb
@@ -1,7 +1,8 @@
 class HeadersMoreNginxModule < Formula
+  desc "Set, add, and clear arbitrary output headers"
   homepage "https://github.com/openresty/headers-more-nginx-module"
-  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.25.tar.gz"
-  sha1 "514bc3df30b24eb0a06533f1ebaa579b898990f5"
+  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.26.tar.gz"
+  sha256 "d6bdc51f9e778a3b23e41a51cae542c1467a1e5e55c1329a7ffc454be084fb6f"
 
   def install
     (share+"headers-more-nginx-module").install Dir["*"]
