c8fe9e1fa80d542dba50ee15debbf277b2034ceb
Denis Denisov <denji0k@gmail.com>
2015-06-20 02:41:43 +0300

    txid-nginx-module f1c197c

--- Formula/txid-nginx-module.rb
+++ Formula/txid-nginx-module.rb
@@ -1,8 +1,9 @@
 class TxidNginxModule < Formula
+  desc "sortable unique id in 20 case-insensitive characters"
   homepage "https://github.com/streadway/ngx_txid"
-  url "https://github.com/streadway/ngx_txid/archive/e7df6b153f.tar.gz"
-  sha1 "8e779f757ddc5e62237c45b925aba20b8e131e99"
-  version "0.1"
+  url "https://github.com/streadway/ngx_txid/archive/f1c197c.tar.gz"
+  version "0.2"
+  sha256 "c5c14172cf23e572d2258bbbbdf09ae7a81a7b6503ce1a0efe0f76260a9a86c5"
 
   def install
     (share+"txid-nginx-module").install Dir["*"]
