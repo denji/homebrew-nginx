5b26c69d91717d3b495b0a60f0d355f20762645f
Denis Denisov <denji0k@gmail.com>
2015-06-20 02:56:55 +0300

    push-stream-nginx-module 0.5.1

--- Formula/push-stream-nginx-module.rb
+++ Formula/push-stream-nginx-module.rb
@@ -1,7 +1,8 @@
 class PushStreamNginxModule < Formula
+  desc "pure stream http push technology"
   homepage "https://github.com/wandenberg/nginx-push-stream-module"
-  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.4.1.tar.gz"
-  sha1 "ee1e5a13fae891713493fd6f30f85358e98b0963"
+  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.5.1.tar.gz"
+  sha256 "a95f31e80120fd7324795b92a0d94c0b43e1265df7fd0f1eba11a337c1e0a626"
 
   def install
     (share+"push-stream-nginx-module").install Dir["*"]
