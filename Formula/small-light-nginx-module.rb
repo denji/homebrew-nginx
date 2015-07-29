91d3d3c4c84369780701328c0d2518ea70c95c44
Tei1988 <github@grifola.xyz>
2015-07-29 14:34:22 +0900

    small-light-module 0.6.10

--- Formula/small-light-nginx-module.rb
+++ Formula/small-light-nginx-module.rb
@@ -1,7 +1,7 @@
 class SmallLightNginxModule < Formula
   homepage "https://github.com/cubicdaiya/ngx_small_light"
-  url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.6.9.tar.gz"
-  sha256 "5d4e3d4fec3968ee57a5fa066118649e2b994b3f3a293896e62bf690b726b7fb"
+  url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.6.10.tar.gz"
+  sha256 "6da8c8c43ff0c20b292b246b512012617bde810d6f5a4fada3723b9764d4b16a"
 
   depends_on "imagemagick"
   depends_on "pcre"
