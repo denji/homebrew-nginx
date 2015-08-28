e82b9d5349bb0fbe7ff5085cda6704bcf979da25
ajayk <ajaykemparaj@gmail.com>
2015-08-27 21:49:22 -0700

    update redis2-nginx-module to v0.12

--- Formula/redis2-nginx-module.rb
+++ Formula/redis2-nginx-module.rb
@@ -1,8 +1,8 @@
 class Redis2NginxModule < Formula
   desc "upstream module for the Redis 2.0 protocol"
   homepage "https://github.com/openresty/redis2-nginx-module"
-  url "https://github.com/openresty/redis2-nginx-module/archive/v0.11.tar.gz"
-  sha256 "877558e21304f413943e3bdf17b97659d8aed7e4bfdfd3ca964921a72de74c05"
+  url "https://github.com/openresty/redis2-nginx-module/archive/v0.12.tar.gz"
+  sha256 "baf5274c815a5f27854747df09ff5bcd6d82e0baa2f224a9db6450d2f3a35a4f"
 
   def install
     (share+"redis2-nginx-module").install Dir["*"]
