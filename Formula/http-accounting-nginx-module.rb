b315dc94c39f6935e9b9b589c2a43e4dafdc399c
Mirocow <mr.mirocow@gmail.com>
2015-10-03 04:31:24 +0300

    http-accounting-nginx-module 0.5

---
+++ Formula/http-accounting-nginx-module.rb
@@ -0,0 +1,10 @@
+class HttpAccountingNginxModule < Formula
+  desc "Add traffic stat function to nginx"
+  homepage "https://github.com/Lax/ngx_http_accounting_module"
+  url "https://github.com/Lax/ngx_http_accounting_module/archive/v0.5.tar.gz"
+  sha256 "f14b8e23b137eebe9e56e84be831c3db3e042b3294a1d99f1ad9d2715e5383f9"
+
+  def install
+    (share+"http-accounting-nginx-module").install Dir["*"]
+  end
+end
