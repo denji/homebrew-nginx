fa7b2956868c7bb17bbc842586a1684dd66943c9
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 12:39:35 +0100

    eval-nginx-module: desc added

--- Formula/eval-nginx-module.rb
+++ Formula/eval-nginx-module.rb
@@ -1,9 +1,10 @@
 class EvalNginxModule < Formula
+  desc "Evaluate memcached/proxy response into vars"
   homepage "https://github.com/vkholodkov/nginx-eval-module"
   url "https://github.com/vkholodkov/nginx-eval-module/archive/1.0.3.tar.gz"
   sha256 "849381433a9020ee1162fa6211b047369fde38dc1a8b5de79f03f8fff2407fe2"
 
   def install
-    (share+"eval-nginx-module").install Dir["*"]
+    (share/"eval-nginx-module").install Dir["*"]
   end
 end
