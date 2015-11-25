6ec4ebc2fbe7a5ee4694de9d1803471de2246bb4
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 12:40:50 +0100

    http-flood-detector-nginx-module: desc added

--- Formula/http-flood-detector-nginx-module.rb
+++ Formula/http-flood-detector-nginx-module.rb
@@ -1,10 +1,11 @@
 class HttpFloodDetectorNginxModule < Formula
+  desc "Detect flood"
   homepage "https://github.com/suehiro/ngx_http_flood_detector_module"
   url "https://github.com/suehiro/ngx_http_flood_detector_module/archive/b66fb66c23.tar.gz"
   version "0.1"
   sha256 "d069eaf0a23d94da6e76d96db2242a731252a258b79b95e6b70bf757b3f125e4"
 
   def install
-    (share+"http-flood-detector-nginx-module").install Dir["*"]
+    (share/"http-flood-detector-nginx-module").install Dir["*"]
   end
 end
