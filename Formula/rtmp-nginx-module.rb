5b3af52710a0a96580cd595be5546988b178679e
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-25 12:43:46 +0100

    rtmp-nginx-module: desc added

--- Formula/rtmp-nginx-module.rb
+++ Formula/rtmp-nginx-module.rb
@@ -1,9 +1,10 @@
 class RtmpNginxModule < Formula
+  desc "NGINX-based Media Streaming Server"
   homepage "https://github.com/arut/nginx-rtmp-module"
   url "https://github.com/arut/nginx-rtmp-module/archive/v1.1.7.tar.gz"
   sha256 "7922b0e3d5f3d9c4b275e4908cfb8f5fb1bfb3ac2df77f4c262cda56df21aab3"
 
   def install
-    (share+"rtmp-nginx-module").install Dir["*"]
+    (share/"rtmp-nginx-module").install Dir["*"]
   end
 end
