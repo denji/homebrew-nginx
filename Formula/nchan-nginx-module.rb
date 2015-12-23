15d487c5b9d3841548112155e879dc6570058a65
Leo P <junk@slact.net>
2015-12-22 19:58:42 -0500

    version bump

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -1,8 +1,8 @@
 class NchanNginxModule < Formula
   desc "flexible, fast pub/sub server with Websocket, EventSource, LongPoll support & more."
   homepage "https://nchan.slact.net"
-  url "https://github.com/slact/nchan/archive/v0.931.tar.gz"
-  sha256 "f60171982db9cddc9f07c441cfe41313e0776ebb8d9ea64fd88d00d34a7e4042"
+  url "https://github.com/slact/nchan/archive/v0.94.tar.gz"
+  sha256 "e4006c5f48399a753b1fc9de061d80b1a339198a8be2c8c460e8a4af40f7c2fc"
 
   def install
     (share+"nchan-nginx-module").install Dir["*"]
