6bcb9fc1128a44c2fcbe9f58bd8f1da740241256
Leo P <junk@slact.net>
2015-12-22 20:47:05 -0500

    description was too long

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -1,5 +1,5 @@
 class NchanNginxModule < Formula
-  desc "flexible, fast pub/sub server with Websocket, EventSource, LongPoll support & more."
+  desc "fast, flexible pub/sub server"
   homepage "https://nchan.slact.net"
   url "https://github.com/slact/nchan/archive/v0.94.tar.gz"
   sha256 "e4006c5f48399a753b1fc9de061d80b1a339198a8be2c8c460e8a4af40f7c2fc"
