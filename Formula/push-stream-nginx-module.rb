a309be3dba9af605d1729eadfbccea3e073259a0
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:21 +0100

    push-stream-nginx-module: bottle is unneeded

--- Formula/push-stream-nginx-module.rb
+++ Formula/push-stream-nginx-module.rb
@@ -4,6 +4,8 @@ class PushStreamNginxModule < Formula
   url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.5.1.tar.gz"
   sha256 "a95f31e80120fd7324795b92a0d94c0b43e1265df7fd0f1eba11a337c1e0a626"
 
+  bottle :unneeded
+
   def install
     (share+"push-stream-nginx-module").install Dir["*"]
   end
