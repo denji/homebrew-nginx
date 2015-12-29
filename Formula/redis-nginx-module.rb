3bebf9605d6363e0d0c7f54b101633e72ced6c00
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:21 +0100

    redis-nginx-module: bottle is unneeded

--- Formula/redis-nginx-module.rb
+++ Formula/redis-nginx-module.rb
@@ -4,6 +4,8 @@ class RedisNginxModule < Formula
   url "https://people.FreeBSD.org/~osa/ngx_http_redis-0.3.7.tar.gz"
   sha256 "9dfc14db81f431fdf3d69f3661a37daf110aef5f9479aa7c88cf362bb5d62604"
 
+  bottle :unneeded
+
   def install
     (share+"redis-nginx-module").install Dir["*"]
   end
