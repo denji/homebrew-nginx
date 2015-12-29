fe00645480408281f91247526eca893222ac7f25
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    php-session-nginx-module: bottle is unneeded

--- Formula/php-session-nginx-module.rb
+++ Formula/php-session-nginx-module.rb
@@ -5,6 +5,8 @@ class PhpSessionNginxModule < Formula
   version "0.4b"
   sha256 "8b2a6d77571657f60bd4a4e411604ddbe9cbe7cdd33f5537923dfe4254770694"
 
+  bottle :unneeded
+
   def install
     (share+"php-session-nginx-module").install Dir["*"]
   end
