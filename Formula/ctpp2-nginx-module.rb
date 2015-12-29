68f6ce8dbe267176a4462e9aa9bab81daedb7ae2
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    ctpp2-nginx-module: bottle is unneeded

--- Formula/ctpp2-nginx-module.rb
+++ Formula/ctpp2-nginx-module.rb
@@ -5,6 +5,8 @@ class Ctpp2NginxModule < Formula
   sha256 "f8adfecc23e2c23af95df8549ef92fd52598b21506a9d9df2278b2605668d5a6"
   head "svn://svn.vbart.ru/ngx_ctpp2/trunk"
 
+  bottle :unneeded
+
   def install
     (share+"ctpp2-nginx-module").install Dir["*"]
   end
