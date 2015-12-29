72c55ea2ca62582f38556746ee773cfb60d87af7
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    ngx-devel-kit: bottle is unneeded

--- Formula/ngx-devel-kit.rb
+++ Formula/ngx-devel-kit.rb
@@ -5,6 +5,8 @@ class NgxDevelKit < Formula
   sha256 "501f299abdb81b992a980bda182e5de5a4b2b3e275fbf72ee34dd7ae84c4b679"
   head "https://github.com/simpl/ngx_devel_kit.git"
 
+  bottle :unneeded
+
   def install
     (share+"ngx-devel-kit").install Dir["*"]
   end
