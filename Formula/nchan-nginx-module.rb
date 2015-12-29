66cea418aa9bc7831d8c1e01ecf5fcf1cb4fa42d
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    nchan-nginx-module: bottle is unneeded

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -4,6 +4,8 @@ class NchanNginxModule < Formula
   url "https://github.com/slact/nchan/archive/v0.95.tar.gz"
   sha256 "c852b82f27e9b228f13cd3806826d922b87d69e755e4019f632e1d7d04301fe9"
 
+  bottle :unneeded
+
   def install
     (share+"nchan-nginx-module").install Dir["*"]
   end
