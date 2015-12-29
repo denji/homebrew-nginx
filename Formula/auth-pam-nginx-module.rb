830148325e5ea1abb32dc8dc324a7966be47804b
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    auth-pam-nginx-module: bottle is unneeded

--- Formula/auth-pam-nginx-module.rb
+++ Formula/auth-pam-nginx-module.rb
@@ -4,6 +4,8 @@ class AuthPamNginxModule < Formula
   url "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.4.tar.gz"
   sha256 "095742c5bcb86f2431e215db785bdeb238d594f085a0ac00d16125876a157409"
 
+  bottle :unneeded
+
   def install
     (share+"auth-pam-nginx-module").install Dir["*"]
   end
