d042bc1e697dc4f2ed4f6853b240fe17039b8d89
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:21 +0100

    ustats-nginx-module: bottle is unneeded

--- Formula/ustats-nginx-module.rb
+++ Formula/ustats-nginx-module.rb
@@ -5,6 +5,8 @@ class UstatsNginxModule < Formula
   version "0.3"
   sha256 "4eb5aadd2129da80d110d6c5a9f6a13485c0a24b5397d183bd24fcbe03125771"
 
+  bottle :unneeded
+
   def install
     (share+"ustats-nginx-module").install Dir["*"]
   end
