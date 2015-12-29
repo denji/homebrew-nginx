fdaf5f6a2f65f71ae3f8bdde4caa4da027980c32
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:21 +0100

    redis2-nginx-module: bottle is unneeded

--- Formula/redis2-nginx-module.rb
+++ Formula/redis2-nginx-module.rb
@@ -4,6 +4,8 @@ class Redis2NginxModule < Formula
   url "https://github.com/openresty/redis2-nginx-module/archive/v0.12.tar.gz"
   sha256 "baf5274c815a5f27854747df09ff5bcd6d82e0baa2f224a9db6450d2f3a35a4f"
 
+  bottle :unneeded
+
   def install
     (share+"redis2-nginx-module").install Dir["*"]
   end
