2814776c5a61dfbe9ab519269843a04886747f83
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:21 +0100

    tcp-proxy-nginx-module: bottle is unneeded

--- Formula/tcp-proxy-nginx-module.rb
+++ Formula/tcp-proxy-nginx-module.rb
@@ -4,6 +4,8 @@ class TcpProxyNginxModule < Formula
   url "https://github.com/yaoweibin/nginx_tcp_proxy_module/archive/v0.4.5.tar.gz"
   sha256 "5225fa70785b14fcdf14a163d01b094c746b70e5ebad7dc35740af4f6d115390"
 
+  bottle :unneeded
+
   def install
     (share+"tcp-proxy-nginx-module").install Dir["*"]
   end
