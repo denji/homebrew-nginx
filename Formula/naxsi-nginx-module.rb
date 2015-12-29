6778cbebef39b615993110b76ec9a8d6ed22d234
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    naxsi-nginx-module: bottle is unneeded

--- Formula/naxsi-nginx-module.rb
+++ Formula/naxsi-nginx-module.rb
@@ -5,6 +5,8 @@ class NaxsiNginxModule < Formula
   sha256 "9cc2c09405bc71f78ef26a8b6d70afcea3fccbe8125df70cb0cfc480133daba5"
   head "https://github.com/nbs-system/naxsi.git"
 
+  bottle :unneeded
+
   def install
     cd "naxsi_src" do
       (share+"naxsi-nginx-module").install Dir["*"]
