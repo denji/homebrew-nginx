a23052ef4b8b4015310a181611c609736e7adb8d
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    mogilefs-nginx-module: bottle is unneeded

--- Formula/mogilefs-nginx-module.rb
+++ Formula/mogilefs-nginx-module.rb
@@ -5,6 +5,8 @@ class MogilefsNginxModule < Formula
   sha256 "7ac230d30907f013dff8d435a118619ea6168aa3714dba62c6962d350c6295ae"
   head "https://github.com/vkholodkov/nginx-mogilefs-module.git"
 
+  bottle :unneeded
+
   def install
     (share+"mogilefs-nginx-module").install Dir["*"]
   end
