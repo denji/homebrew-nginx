0bca43de8603f229c90df9f268766890f65960d2
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-29 14:23:20 +0100

    headers-more-nginx-module: bottle is unneeded

--- Formula/headers-more-nginx-module.rb
+++ Formula/headers-more-nginx-module.rb
@@ -4,6 +4,8 @@ class HeadersMoreNginxModule < Formula
   url "https://github.com/openresty/headers-more-nginx-module/archive/v0.26.tar.gz"
   sha256 "648e450ff48256919e948092e22dc9e17557f55311a669c5c72842a3d5ec1e79"
 
+  bottle :unneeded
+
   def install
     (share+"headers-more-nginx-module").install Dir["*"]
   end
