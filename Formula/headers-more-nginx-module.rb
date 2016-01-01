76480a06e35769c4390d8b0a4bd4fa5dad19964b
Ajay Kemparaj <ajaykemparaj@gmail.com>
2016-01-01 03:03:56 +0200

    headers-more-nginx-module: update to 0.29
    Closes #128

--- Formula/headers-more-nginx-module.rb
+++ Formula/headers-more-nginx-module.rb
@@ -1,8 +1,8 @@
 class HeadersMoreNginxModule < Formula
   desc "Set, add, and clear arbitrary output headers"
   homepage "https://github.com/openresty/headers-more-nginx-module"
-  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.26.tar.gz"
-  sha256 "648e450ff48256919e948092e22dc9e17557f55311a669c5c72842a3d5ec1e79"
+  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.29.tar.gz"
+  sha256 "0a5f3003b5851373b03c542723eb5e7da44a01bf4c4c5f20b4de53f355a28d33"
 
   bottle :unneeded
 
