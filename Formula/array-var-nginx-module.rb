1a648c187d22ecd06ef922b516f0f9ac267cc968
ajayk <ajaykemparaj@gmail.com>
2015-12-31 16:37:43 -0800

    Upgrade openresty to 1.9.7.1 and related modules

--- Formula/array-var-nginx-module.rb
+++ Formula/array-var-nginx-module.rb
@@ -1,8 +1,8 @@
 class ArrayVarNginxModule < Formula
   desc "Support for array variables to nginx config files"
   homepage "https://github.com/openresty/array-var-nginx-module"
-  url "https://github.com/openresty/array-var-nginx-module/archive/v0.03.tar.gz"
-  sha256 "f9892ec9a2e872d8aabeb2234d0d4c66cf85b8dbb2ca5c108a20c3d8c2111189"
+  url "https://github.com/openresty/array-var-nginx-module/archive/v0.04.tar.gz"
+  sha256 "1679eabdbca6df5719ae7dab3e0c0f5eff95044b8ec66ac8bff3b0dbe2fad382"
 
   depends_on "ngx-devel-kit"
 
--- Formula/openresty.rb
+++ Formula/openresty.rb
@@ -1,8 +1,8 @@
 class Openresty < Formula
   desc "Fast web app server"
   homepage "http://openresty.org"
-  url "https://openresty.org/download/ngx_openresty-1.9.3.2.tar.gz"
-  sha256 "9f0af0166e5a32d22cda69db716415bec089a6403a73007c45b2eaa90501e086"
+  url "https://openresty.org/download/ngx_openresty-1.9.7.1.tar.gz"
+  sha256 "38dfc100d6f1efb7aba0b246f94f04c527f8f51b68be22a8a6579a3ebbbd89e9"
 
   # nginx options
   option "without-luajit", "Compile *without* support for the Lua Just-In-Time Compiler"
