66604695bed821f6a6eed4f2e488395a7ba50b4e
Ajay Kemparaj <ajaykemparaj@gmail.com>
2016-01-01 03:02:53 +0200

    lua-nginx-module: update to 0.9.20
    Closes #128

--- Formula/lua-nginx-module.rb
+++ Formula/lua-nginx-module.rb
@@ -1,8 +1,8 @@
 class LuaNginxModule < Formula
   desc "Embed the power of Lua into Nginx"
   homepage "https://github.com/openresty/lua-nginx-module"
-  url "https://github.com/openresty/lua-nginx-module/archive/v0.9.19.tar.gz"
-  sha256 "2b41d4d2627fda76946f194aae6c2b4d89bb59afd792d397c6b481ee82c10eb4"
+  url "https://github.com/openresty/lua-nginx-module/archive/v0.9.20.tar.gz"
+  sha256 "6716001e1387e0f6591026607b7decb9067a85896a8ce3dec94187dd4e679014"
   head "https://github.com/openresty/lua-nginx-module.git"
 
   depends_on "luajit"
