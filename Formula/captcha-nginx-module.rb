7c8765283849b941edee7bd44ff2431c7c834863
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 12:32:03 +0100

    captcha-nginx-module: bottle unneeded

--- Formula/captcha-nginx-module.rb
+++ Formula/captcha-nginx-module.rb
@@ -5,6 +5,8 @@ class CaptchaNginxModule < Formula
   version "0.1"
   sha256 "35e701afd0343a7fe167df6b3d546d4a8589e6d5ff1aa9064a8f9ebd936cc50b"
 
+  bottle :unneeded
+
   depends_on "imagemagick"
 
   def install
