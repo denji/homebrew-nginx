b3768492b706f0805db87582a980cc5f55382f5e
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 12:29:16 +0100

    array-var-nginx-module: bottle unneeded

--- Formula/array-var-nginx-module.rb
+++ Formula/array-var-nginx-module.rb
@@ -1,8 +1,10 @@
 class ArrayVarNginxModule < Formula
-  desc "Support for array variables to nginx config files"
+  desc "Support for arrays to nginx config files"
   homepage "https://github.com/openresty/array-var-nginx-module"
   url "https://github.com/openresty/array-var-nginx-module/archive/v0.04.tar.gz"
   sha256 "1679eabdbca6df5719ae7dab3e0c0f5eff95044b8ec66ac8bff3b0dbe2fad382"
+
+  bottle :unneeded
 
   depends_on "ngx-devel-kit"
 
