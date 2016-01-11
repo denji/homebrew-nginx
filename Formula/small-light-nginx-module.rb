0049afe2a82046a98d753c920b7dd07454c7604e
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 13:43:09 +0100

    small-light-nginx-module: bottle unneeded

--- Formula/small-light-nginx-module.rb
+++ Formula/small-light-nginx-module.rb
@@ -4,6 +4,8 @@ class SmallLightNginxModule < Formula
   url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.6.13.tar.gz"
   sha256 "4f6ace8e8b72ebd28dfd7da66f29d1bbe340a6f8e4157215e2aeec50b3e4c5c7"
 
+  bottle :unneeded
+
   depends_on "imagemagick"
   depends_on "pcre"
   depends_on "pkg-config" => :build
