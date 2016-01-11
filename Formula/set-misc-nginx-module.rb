9a06425fecc9a5acfec83f40a1e4f9198784fe7a
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 12:33:37 +0100

    set-misc-nginx-module: bottle unneeded

--- Formula/set-misc-nginx-module.rb
+++ Formula/set-misc-nginx-module.rb
@@ -1,8 +1,10 @@
 class SetMiscNginxModule < Formula
-  desc "rewrite module md5/sha1, sql/json quoting, and many more"
+  desc "Rewrite module md5/sha1, and many more"
   homepage "https://github.com/openresty/set-misc-nginx-module"
   url "https://github.com/openresty/set-misc-nginx-module/archive/v0.29.tar.gz"
   sha256 "8d280fc083420afb41dbe10df9a8ceec98f1d391bd2caa42ebae67d5bc9295d8"
+
+  bottle :unneeded
 
   depends_on "ngx-devel-kit"
 
