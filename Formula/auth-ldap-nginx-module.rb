bba01480f8437c94daf34ee0e7ff0a2ea921be0e
Denis Denisov <denji0k@gmail.com>
2016-01-16 19:37:18 +0200

    nginx-auth-ldap: kvspb/nginx-auth-ldap@d0f2f82

--- Formula/auth-ldap-nginx-module.rb
+++ Formula/auth-ldap-nginx-module.rb
@@ -1,36 +1,16 @@
 class AuthLdapNginxModule < Formula
   desc "LDAP authentication module for nginx"
   homepage "https://github.com/kvspb/nginx-auth-ldap"
-  url "https://github.com/kvspb/nginx-auth-ldap/archive/be8ff8e.tar.gz"
-  version "be8ff8e"
-  sha256 "02adfa17c7f9bc06ae2914ebe0b14a2965fd05127979826010bf9d9fefbfca8b"
+  url "https://github.com/kvspb/nginx-auth-ldap/archive/d0f2f82.tar.gz"
+  version "d0f2f82"
+  sha256 "7c1628d7e6ebb3a41ec916334014b74acc32dd09a0830752a2d03be0be3223c8"
   head "https://github.com/kvspb/nginx-auth-ldap.git"
 
   bottle :unneeded
 
   depends_on "openldap"
 
-  # undefined reference to "_ber_sockbuf_add_io"
-  # - https://github.com/Homebrew/homebrew-nginx/issues/93
-  # - https://github.com/kvspb/nginx-auth-ldap/issues/26
-  patch :DATA
-
   def install
     (share+"auth-ldap-nginx-module").install Dir["*"]
   end
 end
-
-__END__
-diff --git a/config b/config
-index 7931d74..2420bb6 100644
---- a/config
-+++ b/config
-@@ -5,7 +5,7 @@ NGX_ADDON_SRCS="$NGX_ADDON_SRCS $ngx_addon_dir/ngx_http_auth_ldap_module.c"
- CORE_LIBS="$CORE_LIBS -lldap"
- 
- case "$NGX_PLATFORM" in
--    FreeBSD:*|Linux:*|SunOS:*)
-+    Darwin:*|FreeBSD:*|Linux:*|SunOS:*)
-         CORE_LIBS="$CORE_LIBS -llber"
-     ;;
- esac
