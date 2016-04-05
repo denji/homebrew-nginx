e8cf383ad7b645436436d9c8fe022e1e8f9e5d03
Denis Denisov <denji0k@gmail.com>
2016-04-05 15:45:48 +0300

    array-var-nginx-module 0.05

--- Formula/array-var-nginx-module.rb
+++ Formula/array-var-nginx-module.rb
@@ -1,8 +1,8 @@
 class ArrayVarNginxModule < Formula
   desc "Support for arrays to nginx config files"
   homepage "https://github.com/openresty/array-var-nginx-module"
-  url "https://github.com/openresty/array-var-nginx-module/archive/v0.04.tar.gz"
-  sha256 "1679eabdbca6df5719ae7dab3e0c0f5eff95044b8ec66ac8bff3b0dbe2fad382"
+  url "https://github.com/openresty/array-var-nginx-module/archive/v0.05.tar.gz"
+  sha256 "c949d4be6f3442c8e2937046448dc8d8def25c0e0fa6f4e805144cea45eabe80"
 
   bottle :unneeded
 
