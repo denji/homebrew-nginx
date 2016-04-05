fe564784728abe244da79e77969727eb8dbe919d
Denis Denisov <denji0k@gmail.com>
2016-04-05 15:27:00 +0300

    nginx-rtmp-module: 1.1.7.10

--- Formula/rtmp-nginx-module.rb
+++ Formula/rtmp-nginx-module.rb
@@ -1,8 +1,8 @@
 class RtmpNginxModule < Formula
   desc "NGINX-based Media Streaming Server"
   homepage "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module"
-  url "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/v1.1.7.9.tar.gz"
-  sha256 "3966f96c745024a6d2a3ce97ab14d273e2d2fcbe4afe96848b189ad9a93065ac"
+  url "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/v1.1.7.10.tar.gz"
+  sha256 "0b32d34704d038485d93656dc43e970bbdd9c63bca7ff3b81ad941cde9144fc6"
 
   bottle :unneeded
 
