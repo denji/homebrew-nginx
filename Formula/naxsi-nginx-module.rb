f9c593bf8d9074f2ba1927226977e2dca3c19d60
Denis Denisov <denji@users.noreply.github.com>
2016-04-05 16:28:13 +0300

    naxsi-nginx-module 0.55rc1
    * Added support for RAW_BODY (rules to be matched against the full, raw
      body. Can be useful to match rules against unparsed content : XML,
      serialized java objects etc.)
    * Confirmed support as a dynamic module (introduced in nginx 1.9.11)
    * Better makefile for testing & dev, increased coverage
    * Minor bug-fixes (nbs-system/naxsi#120, nbs-system/naxsi#241,
      nbs-system/naxsi#217, nbs-system/naxsi#231)

--- Formula/naxsi-nginx-module.rb
+++ Formula/naxsi-nginx-module.rb
@@ -1,8 +1,8 @@
 class NaxsiNginxModule < Formula
   desc "NAXSI is an open-source WAF for NGINX"
   homepage "https://github.com/nbs-system/naxsi"
-  url "https://github.com/nbs-system/naxsi/archive/0.54.tar.gz"
-  sha256 "9cc2c09405bc71f78ef26a8b6d70afcea3fccbe8125df70cb0cfc480133daba5"
+  url "https://github.com/nbs-system/naxsi/archive/0.55rc1.tar.gz"
+  sha256 "6353441ee53dca173689b63a78f1c9ac5408f3ed066ddaa3f43fd2795bd43cdd"
   head "https://github.com/nbs-system/naxsi.git"
 
   bottle :unneeded
