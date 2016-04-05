37d3475dff19fb60e82e7316528d45eee2799331
Denis Denisov <denji0k@gmail.com>
2016-04-05 15:49:32 +0300

    ngx-devel-kit: 0.3.0rc1
    feature: this module can now be compiled as a dynamic module with NGINX
    1.9.11+ via the --with-dynamic-module=PATH option of ./configure.

--- Formula/ngx-devel-kit.rb
+++ Formula/ngx-devel-kit.rb
@@ -1,8 +1,8 @@
 class NgxDevelKit < Formula
   desc "Nginx Development Kit"
   homepage "https://github.com/simpl/ngx_devel_kit"
-  url "https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz"
-  sha256 "501f299abdb81b992a980bda182e5de5a4b2b3e275fbf72ee34dd7ae84c4b679"
+  url "https://github.com/simpl/ngx_devel_kit/archive/v0.3.0rc1.tar.gz"
+  sha256 "971207aad81244ea60541574776eb9b8e74606bd57f4e6508cbd7926809d9e75"
   head "https://github.com/simpl/ngx_devel_kit.git"
 
   bottle :unneeded
