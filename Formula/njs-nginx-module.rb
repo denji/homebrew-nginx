18d6446a04c60953b4e61745ab98fa409a87968d
ron <ron@aqicn.org>
2016-05-02 14:24:20 +0800

    Updated after running 'brew audit homebrew/nginx/njs-nginx-module --strict --online'

--- Formula/njs-nginx-module.rb
+++ Formula/njs-nginx-module.rb
@@ -1,13 +1,13 @@
 class NjsNginxModule < Formula
   desc "Nginx NJS module"
   homepage "https://github.com/nginx/njs"
-  version "1.10.0.0.0.20160414.1c50334fbea6-1"
   url "https://github.com/nginx/njs/archive/master/nginx-njs-c4a5f2b.tar.gz"
+  version "1.10.0.0.0.20160414.1c50334fbea6-1"
   sha256 "6c348eec7991361698c4c7c1830388a679c5516bf8a6a25d2d07a3c23642f545"
 
   bottle :unneeded
 
-  #The njs folder is organized is this way:
+  # The njs folder is organized is this way:
   #
   # njs/nginx/: contains the config
   # njs/njs/: javascript vm
@@ -20,5 +20,4 @@ class NjsNginxModule < Formula
   def install
     (share+"njs-nginx-module").install Dir["*"]
   end
-
 end
