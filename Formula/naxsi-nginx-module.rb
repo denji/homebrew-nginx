6e4fdd5c3e49dbdce468e47e756101176f5c1bbe
Denis Denisov <denji0k@gmail.com>
2016-07-11 02:40:48 +0300

    naxsi-nginx-module 0.55rc2

--- Formula/naxsi-nginx-module.rb
+++ Formula/naxsi-nginx-module.rb
@@ -1,8 +1,8 @@
 class NaxsiNginxModule < Formula
   desc "NAXSI is an open-source WAF for NGINX"
   homepage "https://github.com/nbs-system/naxsi"
-  url "https://github.com/nbs-system/naxsi/archive/0.55rc1.tar.gz"
-  sha256 "6353441ee53dca173689b63a78f1c9ac5408f3ed066ddaa3f43fd2795bd43cdd"
+  url "https://github.com/nbs-system/naxsi/archive/0.55rc2.tar.gz"
+  sha256 "fed822e3f507801ce44964908eb1dca8ec58dc0a9bc47f7e7d00c6c4ef97f78b"
   head "https://github.com/nbs-system/naxsi.git"
 
   bottle :unneeded
