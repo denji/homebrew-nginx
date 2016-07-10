0e4b22ae7594c09ed118af44f43731301cdb7b14
Denis Denisov <denji0k@gmail.com>
2016-07-11 02:29:44 +0300

    ngx-devel-kit: 0.3.0

--- Formula/ngx-devel-kit.rb
+++ Formula/ngx-devel-kit.rb
@@ -1,8 +1,8 @@
 class NgxDevelKit < Formula
   desc "Nginx Development Kit"
   homepage "https://github.com/simpl/ngx_devel_kit"
-  url "https://github.com/simpl/ngx_devel_kit/archive/v0.3.0rc1.tar.gz"
-  sha256 "971207aad81244ea60541574776eb9b8e74606bd57f4e6508cbd7926809d9e75"
+  url "https://github.com/simpl/ngx_devel_kit/archive/v0.3.0.tar.gz"
+  sha256 "88e05a99a8a7419066f5ae75966fb1efc409bad4522d14986da074554ae61619"
   head "https://github.com/simpl/ngx_devel_kit.git"
 
   bottle :unneeded
