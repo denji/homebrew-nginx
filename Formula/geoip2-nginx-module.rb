834855fd293ddd95c9680d45d576715c29a48237
ilovezfs <ilovezfs@icloud.com>
2016-08-01 04:54:08 -0700

    geoip2-nginx-module 1.1 (#191)

--- Formula/geoip2-nginx-module.rb
+++ Formula/geoip2-nginx-module.rb
@@ -1,13 +1,12 @@
 class Geoip2NginxModule < Formula
   desc "Nginx GeoIP2 module"
   homepage "https://github.com/leev/ngx_http_geoip2_module"
-  url "https://github.com/leev/ngx_http_geoip2_module/archive/1.0.tar.gz"
-  version "1.0"
-  sha256 "7b1eacf92fe57ee6e33316f07e31bda440f24b3af9ab6b14dd109ac0c348d6c7"
-
-  depends_on "libmaxminddb"
+  url "https://github.com/leev/ngx_http_geoip2_module/archive/1.1.tar.gz"
+  sha256 "efa446c10fa4d80ffe8b5e340d83db62474ae7eba091016032b754a1d17a6769"
 
   bottle :unneeded
+
+  depends_on "libmaxminddb"
 
   def install
     pkgshare.install Dir["*"]
