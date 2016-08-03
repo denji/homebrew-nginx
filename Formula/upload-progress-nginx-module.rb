555921382c9a739544d27fcdc88d40c43b69896b
ilovezfs <ilovezfs@icloud.com>
2016-08-03 15:43:22 -0700

    upload-progress-nginx-module 0.9.2

--- Formula/upload-progress-nginx-module.rb
+++ Formula/upload-progress-nginx-module.rb
@@ -1,8 +1,8 @@
 class UploadProgressNginxModule < Formula
   desc "implementing an upload progress for nginx"
   homepage "http://wiki.nginx.org/HttpUploadProgressModule"
-  url "https://github.com/masterzen/nginx-upload-progress-module/archive/v0.9.1.tar.gz"
-  sha256 "99ec072cca35cd7791e77c40a8ded41a7a8c1111e057be26e55fba2fdf105f43"
+  url "https://github.com/masterzen/nginx-upload-progress-module/archive/v0.9.2.tar.gz"
+  sha256 "b286689355442657650421d8e8398bd4abf9dbbaade65947bb0cb74a349cc497"
   head "https://github.com/masterzen/nginx-upload-progress-module.git"
 
   bottle :unneeded
