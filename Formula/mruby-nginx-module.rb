02c29bc98e7d5032cce6962ccc4b28e4fb4f4d06
ilovezfs <ilovezfs@icloud.com>
2016-08-04 21:08:42 -0700

    mruby-nginx-module 1.18.3 (#193)

--- Formula/mruby-nginx-module.rb
+++ Formula/mruby-nginx-module.rb
@@ -1,8 +1,8 @@
 class MrubyNginxModule < Formula
   desc "Embed the power of MRuby into Nginx"
   homepage "https://github.com/matsumoto-r/ngx_mruby"
-  url "https://github.com/matsumoto-r/ngx_mruby/archive/v1.17.1.tar.gz"
-  sha256 "5a347a5db0f12fc765640b30019c2355d9b26b5cbe0407c834983907ea65ceb7"
+  url "https://github.com/matsumoto-r/ngx_mruby/archive/v1.18.3.tar.gz"
+  sha256 "8b3b8d67209dfdac210bf82fc47aa4744bc4c8de927c38d5d0b48ea3bd1a2a31"
 
   bottle :unneeded
 
