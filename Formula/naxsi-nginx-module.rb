5dea4b54b3d314c98804300532d7a63518db5224
ilovezfs <ilovezfs@icloud.com>
2016-09-17 04:37:50 -0700

    naxsi-nginx-module 0.55.1 (#214)

--- Formula/naxsi-nginx-module.rb
+++ Formula/naxsi-nginx-module.rb
@@ -1,8 +1,8 @@
 class NaxsiNginxModule < Formula
   desc "NAXSI is an open-source WAF for NGINX"
   homepage "https://github.com/nbs-system/naxsi"
-  url "https://github.com/nbs-system/naxsi/archive/0.55.tar.gz"
-  sha256 "144f651d8d7fb17b6dbde703070202a5479238718e1f61c26f24fc29423b98fa"
+  url "https://github.com/nbs-system/naxsi/archive/0.55.1.tar.gz"
+  sha256 "45dd0df7a6b0b6aa9c64eb8c39a8e294d659d87fb18e192cf58f1402f3cdb0a8"
   version_scheme 1
   head "https://github.com/nbs-system/naxsi.git"
 
