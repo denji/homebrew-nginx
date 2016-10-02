6d067c3a50e4d8b31a4a7fe5a64ebf9c76b74b9e
ilovezfs <ilovezfs@icloud.com>
2016-10-02 12:28:42 -0700

    small-light-nginx-module 0.9.0 (#221)

--- Formula/small-light-nginx-module.rb
+++ Formula/small-light-nginx-module.rb
@@ -1,8 +1,8 @@
 class SmallLightNginxModule < Formula
   desc "Dynamic image transformation module"
   homepage "https://github.com/cubicdaiya/ngx_small_light"
-  url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.8.1.tar.gz"
-  sha256 "0aacb02ca2fef6b6ed3aac242f5cda2aeefc010066d38975c2f82d526f5d4cbd"
+  url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.9.0.tar.gz"
+  sha256 "5dde5fb5bc32bb943638c06fb94d7ef30eb4af1145e168e54e91c9bb45456776"
 
   bottle :unneeded
 
