2a2da81a2440411a64475697c5f16124c5a235b7
ilovezfs <ilovezfs@icloud.com>
2016-11-03 22:18:33 -0700

    geoip2-nginx-module 2.0

--- Formula/geoip2-nginx-module.rb
+++ Formula/geoip2-nginx-module.rb
@@ -1,8 +1,8 @@
 class Geoip2NginxModule < Formula
   desc "Nginx GeoIP2 module"
   homepage "https://github.com/leev/ngx_http_geoip2_module"
-  url "https://github.com/leev/ngx_http_geoip2_module/archive/1.1.tar.gz"
-  sha256 "efa446c10fa4d80ffe8b5e340d83db62474ae7eba091016032b754a1d17a6769"
+  url "https://github.com/leev/ngx_http_geoip2_module/archive/2.0.tar.gz"
+  sha256 "ebb4652c4f9a2e1ee31fddefc4c93ff78e651a4b2727d3453d026bccbd708d99"
 
   bottle :unneeded
 
