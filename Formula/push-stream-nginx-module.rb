baaf7aebee91266f085332ed3f7d60df28f04a23
ilovezfs <ilovezfs@icloud.com>
2017-01-14 00:06:47 -0800

    push-stream-nginx-module 0.5.2

--- Formula/push-stream-nginx-module.rb
+++ Formula/push-stream-nginx-module.rb
@@ -1,8 +1,8 @@
 class PushStreamNginxModule < Formula
   desc "pure stream http push technology"
   homepage "https://github.com/wandenberg/nginx-push-stream-module"
-  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.5.1.tar.gz"
-  sha256 "a95f31e80120fd7324795b92a0d94c0b43e1265df7fd0f1eba11a337c1e0a626"
+  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.5.2.tar.gz"
+  sha256 "1d07f38acdb8194bd49344b0ba21de101070de9b8731d27a8d22e928850bc199"
 
   bottle :unneeded
 
