c054cb98fdae66ef7e7add5f0274e3c67283ddd8
Denis Denisov <denji0k@gmail.com>
2017-01-20 12:14:26 +0200

    small-light-nginx-module hotfix

--- Formula/small-light-nginx-module.rb
+++ Formula/small-light-nginx-module.rb
@@ -3,6 +3,7 @@ class SmallLightNginxModule < Formula
   homepage "https://github.com/cubicdaiya/ngx_small_light"
   url "https://github.com/cubicdaiya/ngx_small_light/archive/v0.9.0.tar.gz"
   sha256 "5dde5fb5bc32bb943638c06fb94d7ef30eb4af1145e168e54e91c9bb45456776"
+  revision 1
 
   bottle :unneeded
 
