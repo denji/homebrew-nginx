f9bfbbca0ee04928d36b33c582393bbef24afd97
Leo P <slact@users.noreply.github.com>
2017-03-26 01:11:03 -0400

    nchan 1.1.3 (#270)
    homepage and add support for --HEAD builds

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -1,8 +1,9 @@
 class NchanNginxModule < Formula
   desc "fast, flexible pub/sub server"
-  homepage "https://nchan.slact.net"
-  url "https://github.com/slact/nchan/archive/v1.1.2.tar.gz"
-  sha256 "ff844b922ddfed81b428ada72cdeeb1ac56b8836c3b5cb3c2118784cc163e208"
+  homepage "https://nchan.io"
+  head "https://github.com/slact/nchan.git"
+  url "https://github.com/slact/nchan/archive/v1.1.3.tar.gz"
+  sha256 "8bee9eb6cf69b90f84af2657a9c0648b100e6027007638a895a511fe5a600cdd"
 
   bottle :unneeded
 
