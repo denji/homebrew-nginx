8e4e84847c1bf7b761387a6fcce2b40ea22f15fd
ilovezfs <ilovezfs@icloud.com>
2017-04-03 05:18:48 -0700

    auth-pam-nginx-module 1.5.1

--- Formula/auth-pam-nginx-module.rb
+++ Formula/auth-pam-nginx-module.rb
@@ -1,8 +1,8 @@
 class AuthPamNginxModule < Formula
   desc "PAM for http basic authentication for nginx"
   homepage "https://github.com/stogh/ngx_http_auth_pam_module"
-  url "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.4.tar.gz"
-  sha256 "095742c5bcb86f2431e215db785bdeb238d594f085a0ac00d16125876a157409"
+  url "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.5.1.tar.gz"
+  sha256 "77676842919134af88a7b4bfca4470223e3a00d287d17c0dbdc9a114a685b6e7"
 
   bottle :unneeded
 
