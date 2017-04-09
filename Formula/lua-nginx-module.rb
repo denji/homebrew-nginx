592fd7d17833663269e4722ddc0f81fc5e221078
ilovezfs <ilovezfs@icloud.com>
2017-04-08 23:34:06 -0700

    lua-nginx-module 0.10.8 (#275)

--- Formula/lua-nginx-module.rb
+++ Formula/lua-nginx-module.rb
@@ -1,8 +1,8 @@
 class LuaNginxModule < Formula
   desc "Embed the power of Lua into Nginx"
   homepage "https://github.com/openresty/lua-nginx-module"
-  url "https://github.com/openresty/lua-nginx-module/archive/v0.10.7.tar.gz"
-  sha256 "c21c8937dcdd6fc2b6a955f929e3f4d1388610f47180e60126e6dcab06786f77"
+  url "https://github.com/openresty/lua-nginx-module/archive/v0.10.8.tar.gz"
+  sha256 "d67449c71051b3cc2d6dd60df0ae0d21fca08aa19c9b30c5b95ee21ff38ef8dd"
   head "https://github.com/openresty/lua-nginx-module.git"
 
   bottle :unneeded
