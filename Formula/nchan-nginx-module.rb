38635991020a7164a744565ff24ceff2e28fe611
Leo P <slact@users.noreply.github.com>
2017-05-09 05:19:13 -0400

    nchan-nginx-module 1.1.6 (#290)

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -2,8 +2,8 @@ class NchanNginxModule < Formula
   desc "fast, flexible pub/sub server"
   homepage "https://nchan.io"
   head "https://github.com/slact/nchan.git"
-  url "https://github.com/slact/nchan/archive/v1.1.5.tar.gz"
-  sha256 "b376a79321bd920ac4babb3f3d699198caac017aec24ca333d7361c40760e7c2"
+  url "https://github.com/slact/nchan/archive/v1.1.6.tar.gz"
+  sha256 "df037766e3a152d8cf2a8b644dd10db8cac3679523adc3c063d41ffeb20d43bc"
 
   bottle :unneeded
 
