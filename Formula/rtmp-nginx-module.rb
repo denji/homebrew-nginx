1a344f820c391692f823c00788f82090960af448
Denis Denisov <denji0k@gmail.com>
2017-05-23 08:25:52 +0300

    nginx-rtmp-module: 1.1.7.11-dev (rev 1)
    Resolve #297

--- Formula/rtmp-nginx-module.rb
+++ Formula/rtmp-nginx-module.rb
@@ -4,10 +4,11 @@ class RtmpNginxModule < Formula
   url "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/v1.1.7.10.tar.gz"
   sha256 "0b32d34704d038485d93656dc43e970bbdd9c63bca7ff3b81ad941cde9144fc6"
   version "1.1.7.11-dev"
+  revision 1
   
   patch do
-    url "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/compare/v1.1.7.10...542106e.diff"
-    sha256 "4bce367978d8dc2896432bc517f642626cc006256052957df37b94a6c9eb024b"
+    url "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/compare/v1.1.7.10...d25c56f.diff"
+    sha256 "621d406f20195400603f492ec785ea1c01af04e877bc84c1aeb40487d27dcc13"
   end
 
   bottle :unneeded
