2de62db26d625e01b92cdf4019be3c7db0312968
ilovezfs <ilovezfs@icloud.com>
2017-05-30 03:18:52 -0700

    lua-nginx-module: fix patch checksum

--- Formula/lua-nginx-module.rb
+++ Formula/lua-nginx-module.rb
@@ -6,7 +6,7 @@ class LuaNginxModule < Formula
     sha256 "d67449c71051b3cc2d6dd60df0ae0d21fca08aa19c9b30c5b95ee21ff38ef8dd"
     patch do
       url "https://github.com/openresty/lua-nginx-module/pull/1017.patch"
-      sha256 "0ce12facbf3070f5fed58cae14f5e58c00803733e7e07fcd8577f901b45a17c9"
+      sha256 "93e7bf4460668ad3e5fbfd39f4ca25d29bd7975864b7f42749f94b51346e5166"
     end
   end
   revision 1
