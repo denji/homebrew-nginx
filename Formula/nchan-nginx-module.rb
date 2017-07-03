09e7a2cf968c76a9d4c0fa8ad94b6dcdb3e9b6d5
ilovezfs <ilovezfs@icloud.com>
2017-07-03 16:54:46 -0700

    nchan-nginx-module 1.1.7 (#307)

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -1,9 +1,9 @@
 class NchanNginxModule < Formula
-  desc "fast, flexible pub/sub server"
+  desc "Fast, flexible pub/sub server"
   homepage "https://nchan.io"
+  url "https://github.com/slact/nchan/archive/v1.1.7.tar.gz"
+  sha256 "c8ae7791560ef19a0fec459f424366a09e3baf52ab0fc4938ffce8d962b6abd6"
   head "https://github.com/slact/nchan.git"
-  url "https://github.com/slact/nchan/archive/v1.1.6.tar.gz"
-  sha256 "df037766e3a152d8cf2a8b644dd10db8cac3679523adc3c063d41ffeb20d43bc"
 
   bottle :unneeded
 
@@ -12,6 +12,6 @@ class NchanNginxModule < Formula
   end
 
   test do
-    File.exist?pkgshare
+    File.exist?(pkgshare)
   end
 end
