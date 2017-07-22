ae85512bb4751ba34659915f89045218495716f6
ilovezfs <ilovezfs@icloud.com>
2017-07-21 23:08:17 -0700

    dav-ext-nginx-module 0.1.0 (#309)

--- Formula/dav-ext-nginx-module.rb
+++ Formula/dav-ext-nginx-module.rb
@@ -1,9 +1,8 @@
 class DavExtNginxModule < Formula
   desc "WebDAV missing commands support PROPFIND and OPTIONS"
   homepage "https://github.com/arut/nginx-dav-ext-module"
-  url "https://github.com/arut/nginx-dav-ext-module/archive/v0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "d428a0236c933779cb40ac8c91afb19d5c25a376dc3caab825bfd543e1ee530d"
+  url "https://github.com/arut/nginx-dav-ext-module/archive/v0.1.0.tar.gz"
+  sha256 "6b004eed8ea16ad8de4d304027bf0413cc323a95914e58625a7dc066481aae3a"
 
   bottle :unneeded
 
