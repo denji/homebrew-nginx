0106bc3795509457bbe9c06667e6ac91deb92abf
Denis Denisov <denji0k@gmail.com>
2017-07-27 02:54:03 +0300

    upload-nginx-module 2.255.1

--- Formula/upload-nginx-module.rb
+++ Formula/upload-nginx-module.rb
@@ -1,9 +1,9 @@
 class UploadNginxModule < Formula
   desc "file uploads using multipart/form-data encoding (RFC 1867)"
   homepage "http://wiki.nginx.org/HttpUploadModule"
-  url "https://github.com/vkholodkov/nginx-upload-module/archive/2.2.tar.gz"
-  version "2.2.0"
-  sha256 "036f9fe82bfaef56ee24849d8343712ac3789a45e563f01337a4a6946f968259"
+  url "https://github.com/vkholodkov/nginx-upload-module/archive/70bee48.tar.gz"
+  version "2.255.1"
+  sha256 "5f1c3c2b9858b9cacd877a0f2df9b9263734e9cef8d837b73f0e659c0f7fde2d"
 
   bottle :unneeded
 
