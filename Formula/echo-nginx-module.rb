620856067cb3ca9c35120fc35c5ff7ac3a61b290
ilovezfs <ilovezfs@icloud.com>
2017-08-09 00:54:15 -0700

    echo-nginx-module 0.61 (#314)

--- Formula/echo-nginx-module.rb
+++ Formula/echo-nginx-module.rb
@@ -1,8 +1,8 @@
 class EchoNginxModule < Formula
   desc "Brings echo/sleep/time/exec and more shell-style"
   homepage "https://github.com/openresty/echo-nginx-module"
-  url "https://github.com/openresty/echo-nginx-module/archive/v0.60.tar.gz"
-  sha256 "1077da2229ac7d0a0215e9e6817e297c10697e095010d88f1adbd1add1ce9f4e"
+  url "https://github.com/openresty/echo-nginx-module/archive/v0.61.tar.gz"
+  sha256 "2e6a03032555f5da1bdff2ae96c96486f447da3da37c117e0f964ae0753d22aa"
   head "https://github.com/openresty/echo-nginx-module.git"
 
   bottle :unneeded
