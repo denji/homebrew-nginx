1802b025637ec22e94536f039c6bee9b4b188485
Denis Denisov <denji0k@gmail.com>
2017-08-09 19:05:09 +0300

    1.13.4 (mainline/devel)

--- Formula/nginx-full.rb
+++ Formula/nginx-full.rb
@@ -6,8 +6,8 @@ class NginxFull < Formula
   head "http://hg.nginx.org/nginx/", :using => :hg
 
   devel do
-    url "https://nginx.org/download/nginx-1.13.3.tar.gz"
-    sha256 "5b73f98004c302fb8e4a172abf046d9ce77739a82487e4873b39f9b0dcbb0d72"
+    url "https://nginx.org/download/nginx-1.13.4.tar.gz"
+    sha256 "de21f3c49ba65c611329d8759a63d72e5fcf719bc6f2a3270e2541348ef1fbba"
   end
 
   conflicts_with "nginx", :because => "nginx-full symlink with the name for compatibility with nginx"
