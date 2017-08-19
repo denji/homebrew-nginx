6b246ccd8fa5455f08ed00530408d092ac07ec40
ilovezfs <ilovezfs@icloud.com>
2017-08-19 00:55:06 -0700

    fancyindex-nginx-module 0.4.2 (#315)

--- Formula/fancyindex-nginx-module.rb
+++ Formula/fancyindex-nginx-module.rb
@@ -1,8 +1,8 @@
 class FancyindexNginxModule < Formula
   desc "Fancy indexes module for the Nginx"
   homepage "https://github.com/aperezdc/ngx-fancyindex"
-  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.4.1.tar.gz"
-  sha256 "2b00d8e0ad2a67152a9cee7b7ee67990c742d501412df912baaf1eee9bb6dc71"
+  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.4.2.tar.gz"
+  sha256 "8327150864ca267b735d550d3304030efbbd863fdddfe0a94e970f249a8827ee"
   head "https://github.com/aperezdc/ngx-fancyindex.git"
 
   bottle :unneeded
