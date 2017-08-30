87d55d7761d5d1f498d3ce7f7c40d54b14ed6c22
ilovezfs <ilovezfs@icloud.com>
2017-08-29 19:51:32 -0700

    vod-nginx-module 1.19

--- Formula/vod-nginx-module.rb
+++ Formula/vod-nginx-module.rb
@@ -1,8 +1,8 @@
 class VodNginxModule < Formula
-  desc "drm on the fly for video on demand"
+  desc "DRM on the fly for video on demand"
   homepage "https://github.com/kaltura/nginx-vod-module"
-  url "https://github.com/kaltura/nginx-vod-module/archive/1.18.tar.gz"
-  sha256 "cba0673725f66279671d9dc0ac7bea6ea78cf963ce1b6582320d10a857a818d3"
+  url "https://github.com/kaltura/nginx-vod-module/archive/1.19.tar.gz"
+  sha256 "ea71c02fc9257463a3cb579db7dc2c76fbc4dd1b2657551bf6d78ec722be99bf"
 
   bottle :unneeded
 
