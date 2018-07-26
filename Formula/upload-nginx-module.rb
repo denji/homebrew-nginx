c4b52a7c7f5ff29d75bf5d672c0bc5f568e3a9d3
allard <johan@allardsoft.com>
2018-07-26 20:09:49 +1000

    Latest Upload Module 57bbb0d as of March 18, 2018.
    With http2 support.

--- Formula/upload-nginx-module.rb
+++ Formula/upload-nginx-module.rb
@@ -1,9 +1,9 @@
 class UploadNginxModule < Formula
   desc "file uploads using multipart/form-data encoding (RFC 1867)"
   homepage "http://wiki.nginx.org/HttpUploadModule"
-  url "https://github.com/vkholodkov/nginx-upload-module/archive/70bee48.tar.gz"
-  version "2.255.1"
-  sha256 "5f1c3c2b9858b9cacd877a0f2df9b9263734e9cef8d837b73f0e659c0f7fde2d"
+  url "https://github.com/vkholodkov/nginx-upload-module/archive/57bbb0db23f113f2a8fa2d09d9193927b891fa75.zip"
+  version "2.255.2"
+  sha256 "8786f14d3274a122c0bb72c39431df8bc260e603228182b3c8a5731055ac8da0"
 
   bottle :unneeded
 
