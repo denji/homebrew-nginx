3b6c9b177c525672f83785a8b78d4e1cf5140884
Denis Denisov <denji0k@gmail.com>
2018-08-03 04:52:55 +0300

    redis-nginx-module 0.3.9

--- Formula/redis-nginx-module.rb
+++ Formula/redis-nginx-module.rb
@@ -1,8 +1,8 @@
 class RedisNginxModule < Formula
   desc "keep-alive TCP connections to your backend redis servers"
   homepage "http://wiki.nginx.org/HttpRedis"
-  url "https://people.FreeBSD.org/~osa/ngx_http_redis-0.3.7.tar.gz"
-  sha256 "9dfc14db81f431fdf3d69f3661a37daf110aef5f9479aa7c88cf362bb5d62604"
+  url "https://people.FreeBSD.org/~osa/ngx_http_redis-0.3.9.tar.gz"
+  sha256 "21f87540f0a44b23ffa5df16fb3d788bc90803b255ef14f9c26e3847a6f26f46"
 
   bottle :unneeded
 
