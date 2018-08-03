796583c2423165ef8f758bf95b5f09b4d8cb073a
Denis Denisov <denji0k@gmail.com>
2018-08-03 04:26:15 +0300

    brotli-nginx-module 0.1.2
    https://github.com/eustas/ngx_brotli/releases
    Resolved #343

---
+++ Formula/brotli-nginx-module.rb
@@ -0,0 +1,13 @@
+class BrotliNginxModule < Formula
+  desc "NGINX module for Brotli compression"
+  homepage "https://github.com/eustas/ngx_brotli"
+  url "https://github.com/eustas/ngx_brotli/archive/v0.1.2.tar.gz"
+  sha256 "309af9e96c10e80f1884acea96379980979581adc287ce338f084607bd48c185"
+  head "https://github.com/eustas/ngx_brotli.git"
+
+  bottle :unneeded
+
+  def install
+    pkgshare.install Dir["*"]
+  end
+end
