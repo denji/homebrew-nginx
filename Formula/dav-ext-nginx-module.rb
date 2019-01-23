094225518d1d1cb9f3234ee84dda080b7cdad622
Denis Denisov <denji0k@gmail.com>
2019-01-23 14:09:55 +0200

    dav-ext-nginx-module 3.0.0

--- Formula/dav-ext-nginx-module.rb
+++ Formula/dav-ext-nginx-module.rb
@@ -1,8 +1,8 @@
 class DavExtNginxModule < Formula
   desc "WebDAV missing commands support PROPFIND and OPTIONS"
   homepage "https://github.com/arut/nginx-dav-ext-module"
-  url "https://github.com/arut/nginx-dav-ext-module/archive/v0.1.0.tar.gz"
-  sha256 "6b004eed8ea16ad8de4d304027bf0413cc323a95914e58625a7dc066481aae3a"
+  url "https://github.com/arut/nginx-dav-ext-module/archive/v3.0.0.tar.gz"
+  sha256 "d2499d94d82d4e4eac8425d799e52883131ae86a956524040ff2fd230ef9f859"
 
   bottle :unneeded
 
