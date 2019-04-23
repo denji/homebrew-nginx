5c31b2d7b597569a2bbff92fbe6877f8f308bf17
Denis Denisov <denji0k@gmail.com>
2019-04-23 21:15:00 +0300

    nginx 1.16.0

--- Formula/nginx-full.rb
+++ Formula/nginx-full.rb
@@ -3,8 +3,8 @@ class NginxFull < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.15.12.tar.gz"
-  sha256 "3d5b90aa17de1700709ae4ec6c4d73d87c888b06c510391bf7104b006fdb2abe"
+  url "https://nginx.org/download/nginx-1.16.0.tar.gz"
+  sha256 "4fd376bad78797e7f18094a00f0f1088259326436b537eb5af69b01be2ca1345"
   head "http://hg.nginx.org/nginx/", :using => :hg
 
   conflicts_with "nginx", :because => "nginx-full symlink with the name for compatibility with nginx"
