9d765d93081a046bafe0fe5fb7902169a1212cad
Will Bond <will@wbond.net>
2020-01-06 19:36:02 -0500

    lua-nginx-module 0.10.14 (#375)
    Fixes a segfault when trying to use init_worker_by_lua in newer versions
    of nginx.
    
    I did not update to 0.10.15 since that now requires resty.core as a
    dependency.
    
    References:
     - https://github.com/openresty/lua-nginx-module/issues/1348
     - https://github.com/openresty/lua-nginx-module/pull/1352
     - https://github.com/openresty/lua-nginx-module/pull/1349

--- Formula/lua-nginx-module.rb
+++ Formula/lua-nginx-module.rb
@@ -1,8 +1,8 @@
 class LuaNginxModule < Formula
   desc "Embed the power of Lua into Nginx"
   homepage "https://github.com/openresty/lua-nginx-module"
-  url "https://github.com/openresty/lua-nginx-module/archive/v0.10.13.tar.gz"
-  sha256 "ecea8c3d7f69dd48c6132498ddefb5d83ba9f387fa3d4da14e2abeacdfc8a3ee"
+  url "https://github.com/openresty/lua-nginx-module/archive/v0.10.14.tar.gz"
+  sha256 "9e17e086d0ac74fb72326abb7f2f8274c080b4981cbf358b026307b4088e7148"
   head "https://github.com/openresty/lua-nginx-module.git"
 
   bottle :unneeded
