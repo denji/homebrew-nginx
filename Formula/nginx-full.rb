496a732d5a08bdf2253a782715ab39f49387a6ba
Denis Denisov <denji0k@gmail.com>
2020-01-29 20:42:14 +0200

    nginx 1.17.8
    Changes with nginx 1.17.8                                        21 Jan 2020
    
        *) Feature: variables support in the "grpc_pass" directive.
    
        *) Bugfix: a timeout might occur while handling pipelined requests in an
           SSL connection; the bug had appeared in 1.17.5.
    
        *) Bugfix: in the "debug_points" directive when using HTTP/2.
           Thanks to Daniil Bondarev.

--- Formula/nginx-full.rb
+++ Formula/nginx-full.rb
@@ -3,8 +3,8 @@ class NginxFull < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.17.7.tar.gz"
-  sha256 "b62756842807e5693b794e5d0ae289bd8ae5b098e66538b2a91eb80f25c591ff"
+  url "https://nginx.org/download/nginx-1.17.8.tar.gz"
+  sha256 "97d23ecf6d5150b30e284b40e8a6f7e3bb5be6b601e373a4d013768d5a25965b"
   head "http://hg.nginx.org/nginx/", :using => :hg
 
   conflicts_with "nginx", :because => "nginx-full symlink with the name for compatibility with nginx"
