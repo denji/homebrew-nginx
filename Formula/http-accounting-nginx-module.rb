180b3bcf333f3fb1d99a0b56ae52031204f39ea0
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:10:49 -1000

    http-accounting-nginx-module 2.0 (#393)

--- Formula/http-accounting-nginx-module.rb
+++ Formula/http-accounting-nginx-module.rb
@@ -1,8 +1,8 @@
 class HttpAccountingNginxModule < Formula
   desc "Add traffic stat function to nginx"
   homepage "https://github.com/Lax/ngx_http_accounting_module"
-  url "https://github.com/Lax/ngx_http_accounting_module/archive/v0.5.tar.gz"
-  sha256 "f14b8e23b137eebe9e56e84be831c3db3e042b3294a1d99f1ad9d2715e5383f9"
+  url "https://github.com/Lax/ngx_http_accounting_module/archive/v2.0.tar.gz"
+  sha256 "8c99c5313e5c822aa5683691c8a0641499b2fa8c67f9e55652817042e21f5986"
 
   def install
     pkgshare.install Dir["*"]
