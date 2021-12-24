dda24dae93676729611042e0004783522676049b
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 15:53:15 -1000

    echo-nginx-module 0.62

--- Formula/echo-nginx-module.rb
+++ Formula/echo-nginx-module.rb
@@ -1,8 +1,8 @@
 class EchoNginxModule < Formula
   desc "Brings echo/sleep/time/exec and more shell-style"
   homepage "https://github.com/openresty/echo-nginx-module"
-  url "https://github.com/openresty/echo-nginx-module/archive/v0.61.tar.gz"
-  sha256 "2e6a03032555f5da1bdff2ae96c96486f447da3da37c117e0f964ae0753d22aa"
+  url "https://github.com/openresty/echo-nginx-module/archive/v0.62.tar.gz"
+  sha256 "86f6866baf7297d85feb42ff6d9c212d1de696f3314a53d3a41a4b533a67ab01"
   head "https://github.com/openresty/echo-nginx-module.git"
 
   def install
