53f6fe1e2abd485b6517d039296c7a4dd8105bec
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:08:24 -1000

    mruby-nginx-module 2.2.3 (#395)

--- Formula/mruby-nginx-module.rb
+++ Formula/mruby-nginx-module.rb
@@ -1,8 +1,8 @@
 class MrubyNginxModule < Formula
   desc "Embed the power of MRuby into Nginx"
   homepage "https://github.com/matsumotory/ngx_mruby"
-  url "https://github.com/matsumotory/ngx_mruby/archive/v2.1.0.tar.gz"
-  sha256 "8cc50f7bac46fab803d3ce18857dc3164fbfb733d728d75d0015a14756db0606"
+  url "https://github.com/matsumotory/ngx_mruby/archive/v2.2.3.tar.gz"
+  sha256 "d4812fc0e3550ec672db65d71da73b4b0738323fc7edfb639d73ef34fb2e549e"
 
   depends_on "ngx-devel-kit"
 
