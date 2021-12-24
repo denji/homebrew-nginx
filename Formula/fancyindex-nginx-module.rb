c39fc3f16e8ce0b174a790ce9b6d1bd2f382440c
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:11:05 -1000

    fancyindex-nginx-module 0.5.2 (#390)

--- Formula/fancyindex-nginx-module.rb
+++ Formula/fancyindex-nginx-module.rb
@@ -1,8 +1,8 @@
 class FancyindexNginxModule < Formula
   desc "Fancy indexes module for the Nginx"
   homepage "https://github.com/aperezdc/ngx-fancyindex"
-  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.4.3.tar.gz"
-  sha256 "81698fb0c1ec9f906ce308c055d5d248085caf390f4b92516c1ec93f87c886d4"
+  url "https://github.com/aperezdc/ngx-fancyindex/archive/v0.5.2.tar.gz"
+  sha256 "c3dd84d8ba0b8daeace3041ef5987e3fb96e9c7c17df30c9ffe2fe3aa2a0ca31"
   head "https://github.com/aperezdc/ngx-fancyindex.git"
 
   def install
