a7b8efcbc6615e828cb2716390c8c26e5312aa85
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:09:17 -1000

    set-misc-nginx-module 0.33 (#399)

--- Formula/set-misc-nginx-module.rb
+++ Formula/set-misc-nginx-module.rb
@@ -1,8 +1,8 @@
 class SetMiscNginxModule < Formula
   desc "Rewrite module md5/sha1, and many more"
   homepage "https://github.com/openresty/set-misc-nginx-module"
-  url "https://github.com/openresty/set-misc-nginx-module/archive/v0.32.tar.gz"
-  sha256 "f1ad2459c4ee6a61771aa84f77871f4bfe42943a4aa4c30c62ba3f981f52c201"
+  url "https://github.com/openresty/set-misc-nginx-module/archive/v0.33.tar.gz"
+  sha256 "cd5e2cc834bcfa30149e7511f2b5a2183baf0b70dc091af717a89a64e44a2985"
 
   depends_on "ngx-devel-kit"
 
