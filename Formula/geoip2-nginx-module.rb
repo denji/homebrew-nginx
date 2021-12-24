8271fd15a1fb8fad9f58beee347665dbe19a8010
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:10:36 -1000

    geoip2-nginx-module 3.3 (#391)

--- Formula/geoip2-nginx-module.rb
+++ Formula/geoip2-nginx-module.rb
@@ -1,8 +1,8 @@
 class Geoip2NginxModule < Formula
   desc "Nginx GeoIP2 module"
   homepage "https://github.com/leev/ngx_http_geoip2_module"
-  url "https://github.com/leev/ngx_http_geoip2_module/archive/2.0.tar.gz"
-  sha256 "ebb4652c4f9a2e1ee31fddefc4c93ff78e651a4b2727d3453d026bccbd708d99"
+  url "https://github.com/leev/ngx_http_geoip2_module/archive/3.3.tar.gz"
+  sha256 "41378438c833e313a18869d0c4a72704b4835c30acaf7fd68013ab6732ff78a7"
 
   depends_on "libmaxminddb"
 
