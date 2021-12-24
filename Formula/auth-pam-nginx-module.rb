cadc6c38b448a28327504b92d5708cea9ffdc893
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:08:34 -1000

    auth-pam-nginx-module 1.5.3 (#386)

--- Formula/auth-pam-nginx-module.rb
+++ Formula/auth-pam-nginx-module.rb
@@ -1,8 +1,8 @@
 class AuthPamNginxModule < Formula
   desc "PAM for http basic authentication for nginx"
   homepage "https://github.com/stogh/ngx_http_auth_pam_module"
-  url "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.5.1.tar.gz"
-  sha256 "77676842919134af88a7b4bfca4470223e3a00d287d17c0dbdc9a114a685b6e7"
+  url "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.5.3.tar.gz"
+  sha256 "882018fea8d6955ab3fe294aafa8ebb1fdff4eac313c29583fef02c6de76fae7"
 
   def install
     pkgshare.install Dir["*"]
