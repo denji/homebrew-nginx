de6cda659c4d13d6db5496ea6225c64c2c0335f3
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:10:17 -1000

    eval-nginx-module 1.0.4 (#389)

--- Formula/eval-nginx-module.rb
+++ Formula/eval-nginx-module.rb
@@ -1,8 +1,8 @@
 class EvalNginxModule < Formula
   desc "Evaluate memcached/proxy response into vars"
   homepage "https://github.com/vkholodkov/nginx-eval-module"
-  url "https://github.com/vkholodkov/nginx-eval-module/archive/1.0.3.tar.gz"
-  sha256 "849381433a9020ee1162fa6211b047369fde38dc1a8b5de79f03f8fff2407fe2"
+  url "https://github.com/vkholodkov/nginx-eval-module/archive/1.0.4.tar.gz"
+  sha256 "0042fcfc677e4dab8d5627a46e4818e9c0c93b0aaa26d24ab8a37c9471de06a6"
 
   def install
     pkgshare.install Dir["*"]
