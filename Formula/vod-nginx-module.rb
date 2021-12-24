3c356136391d3958059ef071fc864b9e65ce9c07
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:12:27 -1000

    vod-nginx-module 1.29

--- Formula/vod-nginx-module.rb
+++ Formula/vod-nginx-module.rb
@@ -1,8 +1,8 @@
 class VodNginxModule < Formula
   desc "DRM on the fly for video on demand"
   homepage "https://github.com/kaltura/nginx-vod-module"
-  url "https://github.com/kaltura/nginx-vod-module/archive/1.24.tar.gz"
-  sha256 "b3cad8e3047556955fa028bcc68e7af46d19343942cb7984ad25e6f44d614ddf"
+  url "https://github.com/kaltura/nginx-vod-module/archive/1.29.tar.gz"
+  sha256 "775898874367900f14b6197366501c782e8c86dd2b0c3cc45973abf4e8a9217c"
 
   def install
     pkgshare.install Dir["*"]
