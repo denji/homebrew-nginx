cba4b9688b6fe41a69458cbc98b71b588b02406d
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:10:04 -1000

    cache-purge-nginx-module 2.5.1 (#387)

--- Formula/cache-purge-nginx-module.rb
+++ Formula/cache-purge-nginx-module.rb
@@ -1,8 +1,8 @@
 class CachePurgeNginxModule < Formula
   desc "Purge content from nginx - FastCGI, proxy, SCGI and uWSGI cache"
   homepage "https://github.com/nginx-modules/ngx_cache_purge"
-  url "https://github.com/nginx-modules/ngx_cache_purge/archive/2.4.2.tar.gz"
-  sha256 "067a10ae2a6d623deed5614d9fc55ec9b380d3b6060fd1e32e71c6f955d11cfc"
+  url "https://github.com/nginx-modules/ngx_cache_purge/archive/2.5.1.tar.gz"
+  sha256 "9db968170c301168c4a623bce3fefe8bfd93d676c2006d1a8b2d5eec668cc22c"
 
   def install
     pkgshare.install Dir["*"]
