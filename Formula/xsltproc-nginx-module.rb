f96454c1e4870ec5735f663873fa9e52dedce66a
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-23 16:15:30 -1000

    xsltproc-nginx-module 0.17 (#401)

--- Formula/xsltproc-nginx-module.rb
+++ Formula/xsltproc-nginx-module.rb
@@ -1,8 +1,8 @@
 class XsltprocNginxModule < Formula
   desc "XSLT processor bases on Nginx"
   homepage "https://github.com/yoreek/nginx-xsltproc-module"
-  url "https://github.com/yoreek/nginx-xsltproc-module/archive/v0.16.tar.gz"
-  sha256 "9003d5aa7bff157577d1f8fb5ee070ee52544fc53c48bb9aa0bdd092e5f39bcf"
+  url "https://github.com/yoreek/nginx-xsltproc-module/archive/v0.17.tar.gz"
+  sha256 "cb9157957dffb28c6d15da437396d146b09304901d9d56fa180b9925b039f5eb"
 
   def install
     pkgshare.install Dir["*"]
