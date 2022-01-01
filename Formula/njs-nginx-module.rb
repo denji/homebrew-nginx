3f475de1aebaaaefa9a4e5ec1f9f5b11d8d25020
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-31 23:21:36 -0800

    njs-nginx-module 0.7.1 (#406)

--- Formula/njs-nginx-module.rb
+++ Formula/njs-nginx-module.rb
@@ -1,8 +1,8 @@
 class NjsNginxModule < Formula
   desc "Adds support for njs scripting to nginx"
   homepage "https://github.com/nginx/njs"
-  url "https://github.com/nginx/njs/archive/0.7.0.tar.gz"
-  sha256 "3f0fe7d77600a9c54000ce101dd51f44811964502641ae505e38c1bbf595e6f1"
+  url "https://github.com/nginx/njs/archive/0.7.1.tar.gz"
+  sha256 "f5493b444ef54f1edba85c7adcbb1132e61c36d47de8f7a8d351965cad6d5486"
   head "https://github.com/nginx/njs.git", branch: "master"
 
   depends_on "pcre" => :build
