351ce89b993f773ee1388bee160bc0ab41c499eb
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-31 20:56:50 -0800

    nchan-nginx-module 1.2.15

--- Formula/nchan-nginx-module.rb
+++ Formula/nchan-nginx-module.rb
@@ -1,8 +1,8 @@
 class NchanNginxModule < Formula
   desc "Fast, flexible pub/sub server"
   homepage "https://nchan.io"
-  url "https://github.com/slact/nchan/archive/v1.2.14.tar.gz"
-  sha256 "d4280b83524ec0bbd794ee46e347f990fc1063e1849e41755c128b6e07371ac9"
+  url "https://github.com/slact/nchan/archive/v1.2.15.tar.gz"
+  sha256 "dbb05bec6a4cf6ad1c1a4e613a2c832669c0eb87847e70ab26bc6c590361a818"
   head "https://github.com/slact/nchan.git", branch: "master"
 
   def install
