5464f594af2f18ea8e58c538dcb3452b1cc921ea
Denis Denisov <denji@users.noreply.github.com>
2022-10-19 15:38:37 +0300

    brotli-nginx-module 1.0.0rc
    Resolved #413

--- Formula/brotli-nginx-module.rb
+++ Formula/brotli-nginx-module.rb
@@ -1,9 +1,9 @@
 class BrotliNginxModule < Formula
   desc "NGINX module for Brotli compression"
-  homepage "https://github.com/eustas/ngx_brotli"
-  url "https://github.com/eustas/ngx_brotli/archive/v0.1.2.tar.gz"
-  sha256 "309af9e96c10e80f1884acea96379980979581adc287ce338f084607bd48c185"
-  head "https://github.com/eustas/ngx_brotli.git", branch: "master"
+  homepage "https://github.com/google/ngx_brotli"
+  url "https://github.com/google/ngx_brotli/archive/refs/tags/v1.0.0rc.tar.gz"
+  sha256 "c85cdcfd76703c95aa4204ee4c2e619aa5b075cac18f428202f65552104add3b"
+  head "https://github.com/google/ngx_brotli.git", branch: "master"
 
   def install
     pkgshare.install Dir["*"]
