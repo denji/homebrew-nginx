a9588322b358b36196cb3ebe264f9f6331477b46
Denis Denisov <denji@users.noreply.github.com>
2022-10-24 18:04:57 +0300

    vod-nginx-module 1.30
    - support nginx 1.23.0
    - make max_frame_count configurable
    - output version as a label in prom format status
    - bug fixes
    
    Resolved #414

--- Formula/vod-nginx-module.rb
+++ Formula/vod-nginx-module.rb
@@ -1,8 +1,8 @@
 class VodNginxModule < Formula
   desc "DRM on the fly for video on demand"
   homepage "https://github.com/kaltura/nginx-vod-module"
-  url "https://github.com/kaltura/nginx-vod-module/archive/1.29.tar.gz"
-  sha256 "775898874367900f14b6197366501c782e8c86dd2b0c3cc45973abf4e8a9217c"
+  url "https://github.com/kaltura/nginx-vod-module/archive/1.30.tar.gz"
+  sha256 "db1a7b31b4109eff32519f6624eaa54815db79f8e8b671bd24e5906ea5b3b905"
 
   def install
     pkgshare.install Dir["*"]
