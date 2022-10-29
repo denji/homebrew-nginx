73a5bc36e893cd1450e2b4e9d2ddbf57894eba62
Denis Denisov <denji@users.noreply.github.com>
2022-10-29 06:23:30 +0300

    headers-more-nginx-module 0.34
    * bugfix: nginx crash when accessing uninitialized pointer.
    * bugfix: update handling of multiple headers changed in nginx 1.23.0.
    * bugfix: fixed build error with nginx >= 1.23.0
    * doc: update the description of nginx compatibility. (#131)
    * doc: we now work with nginx 1.17.x (up to 1.17.8 at least).
    * doc: updated the nginx compatibility list.
    
    Resolved #415

--- Formula/headers-more-nginx-module.rb
+++ Formula/headers-more-nginx-module.rb
@@ -1,8 +1,8 @@
 class HeadersMoreNginxModule < Formula
   desc "Set, add, and clear arbitrary output headers"
   homepage "https://github.com/openresty/headers-more-nginx-module"
-  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.33.tar.gz"
-  sha256 "a3dcbab117a9c103bc1ea5200fc00a7b7d2af97ff7fd525f16f8ac2632e30fbf"
+  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.34.tar.gz"
+  sha256 "0c0d2ced2ce895b3f45eb2b230cd90508ab2a773299f153de14a43e44c1209b3"
 
   def install
     pkgshare.install Dir["*"]
