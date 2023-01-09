bd99c61443cce6b26232f875d097a978e241da7f
Denis Denisov <denji@users.noreply.github.com>
2023-01-09 12:53:21 +0200

    Update subs-filter-nginx-module.rb
    PCRE2 support added yaoweibin/ngx_http_substitutions_filter_module@cc494d7
    Resolved #416

--- Formula/subs-filter-nginx-module.rb
+++ Formula/subs-filter-nginx-module.rb
@@ -1,7 +1,9 @@
 class SubsFilterNginxModule < Formula
   desc "Regular expression and fixed string substitutions"
   homepage "https://github.com/yaoweibin/ngx_http_substitutions_filter_module"
-  url "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/v0.6.4.tar.gz"
+  url "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/e12e965.tar.gz"
+  version "0.6.4-p1"
+
   sha256 "ed4ddbcf0c434f4a1e97b61251a63ace759792764bd5cb79ff20efe348db8db3"
 
   def install
