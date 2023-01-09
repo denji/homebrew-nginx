b440965ac258ce526021a8282a9b756c7d70035d
Denis Denisov <denji@users.noreply.github.com>
2023-01-09 12:55:41 +0200

    subs-filter-nginx-module 0.6.4-p1

--- Formula/subs-filter-nginx-module.rb
+++ Formula/subs-filter-nginx-module.rb
@@ -3,8 +3,7 @@ class SubsFilterNginxModule < Formula
   homepage "https://github.com/yaoweibin/ngx_http_substitutions_filter_module"
   url "https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/e12e965.tar.gz"
   version "0.6.4-p1"
-
-  sha256 "ed4ddbcf0c434f4a1e97b61251a63ace759792764bd5cb79ff20efe348db8db3"
+  sha256 "da2a26aa75884e1216941c7291b6e8b7fc4dd7163700a04191b92060b61fabb3"
 
   def install
     pkgshare.install Dir["*"]
