de5130b4d6fa08bec26630265be5f1f7d8392ce1
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 04:03:40 +0200

    auth-ldap-nginx-module kvspb/nginx-auth-ldap@83c059b

--- Formula/auth-ldap-nginx-module.rb
+++ Formula/auth-ldap-nginx-module.rb
@@ -1,9 +1,9 @@
 class AuthLdapNginxModule < Formula
   desc "LDAP authentication module for nginx"
   homepage "https://github.com/kvspb/nginx-auth-ldap"
-  url "https://github.com/kvspb/nginx-auth-ldap/archive/42d195d.tar.gz"
-  version "42d195d"
-  sha256 "80d6cce9a9877d51dec2f85a11ce7cd25edbd2d605c28bc28687ecc5695229ee"
+  url "https://github.com/kvspb/nginx-auth-ldap/archive/83c059b.tar.gz"
+  version "83c059b"
+  sha256 "690c4e5bdb2ae19b0fee75cd356d18013468db472616b6097a5a0bbe346c8464"
   head "https://github.com/kvspb/nginx-auth-ldap.git", branch: "master"
 
   depends_on "openldap"
