2957ff953526ce4245873eb97a13d90c69f9c2fc
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 04:13:43 +0200

    geoip2-nginx-module 3.4

--- Formula/geoip2-nginx-module.rb
+++ Formula/geoip2-nginx-module.rb
@@ -1,8 +1,8 @@
 class Geoip2NginxModule < Formula
   desc "Nginx GeoIP2 module"
   homepage "https://github.com/leev/ngx_http_geoip2_module"
-  url "https://github.com/leev/ngx_http_geoip2_module/archive/3.3.tar.gz"
-  sha256 "41378438c833e313a18869d0c4a72704b4835c30acaf7fd68013ab6732ff78a7"
+  url "https://github.com/leev/ngx_http_geoip2_module/archive/3.4.tar.gz"
+  sha256 "ad72fc23348d715a330994984531fab9b3606e160483236737f9a4a6957d9452"
 
   depends_on "libmaxminddb"
 
