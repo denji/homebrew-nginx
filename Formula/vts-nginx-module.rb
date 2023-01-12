f91a19451114de164da9ed616c6d22a3b9d36539
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 04:12:55 +0200

    vts-nginx-module 0.2.1

--- Formula/vts-nginx-module.rb
+++ Formula/vts-nginx-module.rb
@@ -1,8 +1,8 @@
 class VtsNginxModule < Formula
   desc "Nginx virtual host traffic status module"
   homepage "https://github.com/vozlt/nginx-module-vts"
-  url "https://github.com/vozlt/nginx-module-vts/archive/v0.1.18.tar.gz"
-  sha256 "17ea41d4083f6d1ab1ab83dad9160eeca66867abe16c5a0421f85a39d7c84b65"
+  url "https://github.com/vozlt/nginx-module-vts/archive/v0.2.1.tar.gz"
+  sha256 "1a63d78d3ae4df18b3a57a616eeee006cdc0bd71aa7ea2947046004b123704f4"
   head "https://github.com/vozlt/nginx-module-vts.git", branch: "master"
 
   def install
