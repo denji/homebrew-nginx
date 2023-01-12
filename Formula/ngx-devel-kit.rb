303562592573b45d15270c80d9432d0e10f3a3d7
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 03:54:09 +0200

    ngx-devel-kit 0.3.2

--- Formula/ngx-devel-kit.rb
+++ Formula/ngx-devel-kit.rb
@@ -1,8 +1,8 @@
 class NgxDevelKit < Formula
   desc "Nginx Development Kit"
   homepage "https://github.com/simpl/ngx_devel_kit"
-  url "https://github.com/simpl/ngx_devel_kit/archive/v0.3.1.tar.gz"
-  sha256 "0e971105e210d272a497567fa2e2c256f4e39b845a5ba80d373e26ba1abfbd85"
+  url "https://github.com/simpl/ngx_devel_kit/archive/v0.3.2.tar.gz"
+  sha256 "aa961eafb8317e0eb8da37eb6e2c9ff42267edd18b56947384e719b85188f58b"
   head "https://github.com/simpl/ngx_devel_kit.git", branch: "master"
 
   def install
