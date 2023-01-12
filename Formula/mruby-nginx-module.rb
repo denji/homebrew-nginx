a8c060b4f5ddbc142b1a00f8cb4773aa785b2fc7
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 03:53:00 +0200

    mruby-nginx-module 2.2.5

--- Formula/mruby-nginx-module.rb
+++ Formula/mruby-nginx-module.rb
@@ -1,8 +1,8 @@
 class MrubyNginxModule < Formula
   desc "Embed the power of MRuby into Nginx"
   homepage "https://github.com/matsumotory/ngx_mruby"
-  url "https://github.com/matsumotory/ngx_mruby/archive/v2.2.4.tar.gz"
-  sha256 "884f67430b00289a3b482fc8e64efffe18d09b97c9ff84ff483e5087dfc776c0"
+  url "https://github.com/matsumotory/ngx_mruby/archive/v2.2.5.tar.gz"
+  sha256 "257c3e0b4c75c0fd171cf71c70a0d9c52b55391e30005ba8b562d7df4d8e0cc8"
 
   depends_on "ngx-devel-kit"
 
