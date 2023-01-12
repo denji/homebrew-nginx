3ab4994e8e388bc47720f45786001270fcb95403
Denis Denisov <denji@users.noreply.github.com>
2023-01-12 04:15:28 +0200

    auth-digest-nginx-module samizdatco/nginx-http-auth-digest@823853c

--- Formula/auth-digest-nginx-module.rb
+++ Formula/auth-digest-nginx-module.rb
@@ -1,9 +1,9 @@
 class AuthDigestNginxModule < Formula
   desc "Digest Authentication for Nginx"
   homepage "https://github.com/samizdatco/nginx-http-auth-digest"
-  url "https://github.com/samizdatco/nginx-http-auth-digest/archive/cd86418.tar.gz"
-  version "0.2.2"
-  sha256 "fe683831f832aae4737de1e1026a4454017c2d5f98cb88b08c5411dc380062f8"
+  url "https://github.com/samizdatco/nginx-http-auth-digest/archive/823853c.tar.gz"
+  version "823853c"
+  sha256 "5bd3e208933ba973b550cca9fa280888802af3f82049cf8b2708c1bc21e3d6e7"
 
   def install
     pkgshare.install Dir["*"]
