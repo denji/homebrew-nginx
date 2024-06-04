5a9b94d9907a4d26e09ce9ef1ea31796db3b8c9c
Denis Denisov <denji@users.noreply.github.com>
2024-06-05 00:45:24 +0300

    brotli-nginx-module (brotli v1.1.0)
    google/ngx_brotli@a71f931
    https://github.com/google/brotli/releases/tag/v1.1.0

--- Formula/brotli-nginx-module.rb
+++ Formula/brotli-nginx-module.rb
@@ -1,8 +1,9 @@
 class BrotliNginxModule < Formula
   desc "NGINX module for Brotli compression"
   homepage "https://github.com/google/ngx_brotli"
-  url "https://github.com/google/ngx_brotli/archive/refs/tags/v1.0.0rc.tar.gz"
-  sha256 "c85cdcfd76703c95aa4204ee4c2e619aa5b075cac18f428202f65552104add3b"
+  url "https://github.com/google/ngx_brotli.git",
+      revision: "a71f9312c2deb28875acc7bacfdd5695a111aa53"
+  version "1.1.0rc"
   head "https://github.com/google/ngx_brotli.git", branch: "master"
 
   def install
